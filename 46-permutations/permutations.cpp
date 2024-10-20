e74a03591793f4763bd62301ee72327eab3698b4
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-10-21 01:44:13 +0530

    Time: 1 ms (61.01%) | Memory: 9.2 MB (58.01%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,3 @@
-// class Solution {
-// private:
-// void perm(int pos, vector<int> &ans){
-//     if(pos>=  ans.size()){
-//         ans.push_back(str);
-//     }
-//     for(int i = pos; i<str.size(); i++){
-//         swap(str[i], str[pos]);
-//         perm(pos+1, str, ans);
-//         swap(str[i], str[pos]); // backtracking hai. ISSi ko back ttacking bolte hai malik
-//     }
-// }
-// public:
-//     vector<vector<int>> permute(vector<int>& nums) {
-//         vector<int>ans;
-//         perm(0, int , ans);
-//         sort(ans.being(), sort.end());
-//         return ans;
-//     }
-// };
-
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 private:
     void perm(int pos, vector<int> &nums, vector<vector<int>> &ans) {
@@ -31,7 +8,7 @@ private:
         for (int i = pos; i < nums.size(); i++) {
             swap(nums[i], nums[pos]);
             perm(pos + 1, nums, ans);
-            swap(nums[i], nums[pos]);
+            swap(nums[i], nums[pos]); // backtracking hai. ISSi ko back ttacking bolte hai malik
         }
     }
 public:
