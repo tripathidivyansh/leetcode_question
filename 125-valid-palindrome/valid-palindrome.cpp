c826c7b67c17fc57a3f68fd7379758570777f065
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-10-20 16:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (82.07%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -19,7 +19,7 @@ public:
         int left = 0;
         int right = s.size()-1;
 
-        while(left<= right){
+        while(left< right){
             if(!isalnum(s[left])){
                 left++;
             }else if(!isalnum(s[right])){
