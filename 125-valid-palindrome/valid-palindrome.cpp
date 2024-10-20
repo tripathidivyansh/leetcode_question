370096e04fb870fcb104e65c2582f7091d459489
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-10-20 16:00:05 +0530

    Time: 2 ms (92.04%) | Memory: 8.9 MB (82.07%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     bool isPalindromeHelper(int l, int r, string s){
+
+//         if(l>= r) return true;
+//         if(!isalnum(s[l]))return isPalindromeHelper(l+1,r,s);
+//         if(!isalnum(s[r]))return isPalindromeHelper(l,r-1,s);
+//         if(tolower(s[l]) != tolower(s[r])) return false;
+//         return isPalindromeHelper(l+1, r-1, s);
+//     }
+//     bool isPalindrome(string s) {
+//         return isPalindromeHelper(0, s.size()-1, s);
+//     }
+// };
+
+class Solution{
+public:
+    bool isPalindrome(string s){
+        int left = 0;
+        int right = s.size()-1;
+
+        while(left<= right){
+            if(!isalnum(s[left])){
+                left++;
+            }else if(!isalnum(s[right])){
+                right--;
+            }else if(tolower(s[left]) != tolower(s[right])){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
