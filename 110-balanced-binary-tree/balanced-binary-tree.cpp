f2cf24748cac1eb65496f19c5736694532aeaaad
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-10-27 19:21:13 +0530

    Time: 75 ms (5.14%) | Memory: 21.8 MB (69.31%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,51 +11,30 @@
  */
 
 
-// class Solution {
-// private:
-//     int height(struct TreeNode* node) {
-//         if (node == NULL) {
-//             return 0;
-//         }
-//         int left = height(node->left);
-//         int right = height(node->right);
-
-//         return max(left, right) + 1;
-//     }
-
-// public:
-//     bool isBalanced(TreeNode* root) {
-//         if (root == NULL) {
-//             return true;
-//         }
-
-//         bool leftBalanced = isBalanced(root->left);
-//         bool rightBalanced = isBalanced(root->right);
-
-//         bool heightDiff = abs(height(root->left) - height(root->right)) <= 1;
-
-//         return leftBalanced && rightBalanced && heightDiff;
-//     }
-// };
-
 class Solution {
 private:
-    int height(TreeNode* node) {
+    int height( TreeNode* node) {
         if (node == NULL) {
             return 0;
         }
         int left = height(node->left);
         int right = height(node->right);
+
         return max(left, right) + 1;
     }
+
 public:
     bool isBalanced(TreeNode* root) {
         if (root == NULL) {
             return true;
         }
+
         bool leftBalanced = isBalanced(root->left);
         bool rightBalanced = isBalanced(root->right);
-        bool diff = abs(height(root->left) - height(root->right)) <= 1;
-        return leftBalanced && rightBalanced && diff;
+
+        bool heightDiff = abs(height(root->left) - height(root->right)) <= 1;
+
+        return leftBalanced && rightBalanced && heightDiff;
     }
 };
+
