7ede8094d04f514b66ec064e0d3d8931324eae5f
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-02 13:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (77.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,14 +1,49 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         vector<int> result = {-1, -1};
+//         for(int i = 0; i<n; i++){
+//             if(nums[i] == target){
+//                 if(result[0] == -1){
+//                     result[0] = i;
+//                 }
+//                 result[1] = i;
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
         vector<int> result = {-1, -1};
-        for(int i = 0; i<n; i++){
-            if(nums[i] == target){
-                if(result[0] == -1){
-                    result[0] = i;
-                }
-                result[1] = i;
+        int n = nums.size();
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return result;
