8746d44bd0d7e32f579d3d02c5d6ca2937522936
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-04 14:44:34 +0530

    Time: 8 ms (48.38%) | Memory: 18 MB (54.79%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,14 +1,32 @@
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         vector<int>ans;
+//         int n = matrix.size();
+//         for(int i =0; i<n; i++){
+//             for(int j = 0; j<n; j++){
+//                 ans.push_back(matrix[i][j]);
+//             }
+//         }
+//         sort(ans.begin(), ans.end());
+//         return ans[k-1];
+//     }
+// };
+
+
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        vector<int>ans;
+        priority_queue<int>maxHeap;
         int n = matrix.size();
-        for(int i =0; i<n; i++){
+        for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
-                ans.push_back(matrix[i][j]);
+                maxHeap.push(matrix[i][j]);
+                if(maxHeap.size() > k){
+                    maxHeap.pop();
+                }
             }
         }
-        sort(ans.begin(), ans.end());
-        return ans[k-1];
+        return maxHeap.top();
     }
 };
