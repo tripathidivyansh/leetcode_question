080a8e534cc4ceeec426a4bd469615548d96a2d2
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-04 18:01:59 +0530

    Time: 50 ms (37.10%) | Memory: 32.2 MB (48.45%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,17 +1,48 @@
+// class Solution {
+// public:
+//     bool isNStraightHand(vector<int>& hand, int groupSize) {
+//         int n = hand.size();
+//         map<int, int>mp;
+//         for(int x : hand){
+//             mp[x]++;
+
+//         }
+//         for(auto it:mp){
+//             if(mp[it.first] > 0){
+//                 for(int i = 1; i<groupSize; i++){
+//                     mp[it.first + i] -= mp[it.first];
+//                     if(mp[it.first + i] < 0){
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
-        map<int, int>mp;
-        for(int x : hand){
-            mp[x]++;
-
+        if (n % groupSize != 0) {
+            return false;
         }
-        for(auto it:mp){
-            if(mp[it.first] > 0){
-                for(int i = 1; i<groupSize; i++){
-                    mp[it.first + i] -= mp[it.first];
-                    if(mp[it.first + i] < 0){
+        map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[hand[i]]++;
+        }
+        for (map<int, int>::iterator it = mp.begin(); it != mp.end(); ++it) {
+            int startCard = it->first;
+            int count = it->second;
+
+            if (count > 0) {
+                for (int i = 1; i < groupSize; i++) {
+                    int nextCard = startCard + i;
+                    mp[nextCard] -= count;
+                    if (mp[nextCard] < 0) {
                         return false;
                     }
                 }
