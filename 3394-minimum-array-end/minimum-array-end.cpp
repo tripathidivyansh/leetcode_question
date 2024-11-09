6511d52cd56534cf663f46e82e836ba6f5b816df
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-09 23:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.66%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,10 +1,29 @@
+// class Solution {
+// public:
+//     long long minEnd(int n, int x) {
+//         long long min = x;
+//         for(int i = 1; i<n; i++){
+//             min = (min+1)|x;
+//         }
+//         return min;
+//     }
+// };
+
 class Solution {
 public:
-    long long minEnd(int n, int x) {
-        long long min = x;
-        for(int i = 1; i<n; i++){
-            min = (min+1)|x;
+    long minEnd(int n, int x) {
+        long result = x;
+        long remaining = n - 1;
+        long position = 1;
+        
+        while (remaining) {
+            if (!(x & position)) {
+                result |= (remaining & 1) * position;
+                remaining >>= 1;
+            }
+            position <<= 1;
         }
-        return min;
+        
+        return result;
     }
 };
