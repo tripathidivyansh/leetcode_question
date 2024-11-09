02f6053e34747d37c16af3bfbcbf176479dee9ae
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-09 19:38:39 +0530

    Time: 27 ms (78.90%) | Memory: 59.2 MB (77.34%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,19 +1,27 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end(), std::greater<int>());
+//         int n = nums.size();
+//         priority_queue<int, vector<int>, greater<int>>pq;
+//         for(auto x: nums){
+//             pq.push(x);
+//             if(pq.size() > k){
+//                 pq.pop();
+//             }
+//         }
+//         return pq.top();
+//     }
+// };
+
+
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), std::greater<int>());
-        int n = nums.size();
-        priority_queue<int, vector<int>, greater<int>>pq;
-        for(auto x: nums){
-            pq.push(x);
-            if(pq.size() > k){
-                pq.pop();
-            }
-        }
-        return pq.top();
+    int findKthLargest(std::vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end(), std::greater<int>());
+        return nums[k-1];
     }
 };
-
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
