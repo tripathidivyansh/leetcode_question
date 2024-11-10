7efdf4445085dedcecf82a4d89185f3ebd3bf272
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-10 21:47:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (22.23%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,23 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-private:
-    int height(TreeNode* node, int &diameterOfBinaryTree){
-        if(node == NULL){
-            return 0;
-        }
-        int leftHeight = height(node->left, diameterOfBinaryTree);
-        int rightHeight = height(node->right, diameterOfBinaryTree);
+// class Solution {
+// private:
+//     int height(TreeNode* node, int &diameterOfBinaryTree){
+//         if(node == NULL){
+//             return 0;
+//         }
+//         int leftHeight = height(node->left, diameterOfBinaryTree);
+//         int rightHeight = height(node->right, diameterOfBinaryTree);
 
-        diameterOfBinaryTree = max(diameterOfBinaryTree, leftHeight + rightHeight);
-        return  max(leftHeight, rightHeight) + 1;
-    }
+//         diameterOfBinaryTree = max(diameterOfBinaryTree, leftHeight + rightHeight);
+//         return  max(leftHeight, rightHeight) + 1;
+//     }
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         int diameterOfBinaryTree = 0;
+//         height(root, diameterOfBinaryTree);
+
+//         return diameterOfBinaryTree;
+//     }
+// };
+
+class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameterOfBinaryTree = 0;
-        height(root, diameterOfBinaryTree);
+        int maxi = 0;
+        findmax(root, maxi);
+        return maxi;
+    }
 
-        return diameterOfBinaryTree;
+private:
+    int findmax(TreeNode* node, int& maxi) {
+        if (node == NULL) return 0;
+
+        int leftheight = findmax(node->left, maxi);
+        int rightheight = findmax(node->right, maxi);
+
+        maxi = max(maxi, leftheight + rightheight);
+
+        return 1 + max(leftheight, rightheight);
     }
 };
