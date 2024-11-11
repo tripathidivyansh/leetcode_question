d7ff1676de5448a2021086abb516beee7bc97366
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-11 21:08:46 +0530

    Time: 6 ms (31.82%) | Memory: 17.5 MB (57.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,32 +1,34 @@
 // class Solution {
 // public:
 //     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
 //         int n = nums.size();
-//         vector<int>ans;
-//         unordered_map<int, int>mp;
-//         for(auto x : nums){
+//         vector<int> ans;
+//         unordered_map<int, int> mp;
+
+//         for (auto x : nums) {
 //             mp[x]++;
 //         }
-//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-        
-//         for(auto x : mp){
-//             pq.push(make_pair(x.second, x.first));
-//             if(pq.size() > k){
-//                 pq.pop();
-//             }
-//             while(!pq.empty()){
-//                 pair<int, int> temp = pq.top();
-//                 pq.pop();
-//                 ans.push_back(temp.second);
-//             }
 
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+//         for (auto& x : mp) {
+//             pq.push(make_pair(x.second, x.first));
+//             if (pq.size() > k) {
+//                 pq.pop();
+//             }
 //         }
 
-//     }
+//         while (!pq.empty()) {
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
 
+//         reverse(ans.begin(), ans.end());
+
+//         return ans;
 //     }
 // };
+
 
 class Solution {
 public:
