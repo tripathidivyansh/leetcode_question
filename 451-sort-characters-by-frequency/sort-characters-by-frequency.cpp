9add9147205773e04fb1c6fcbc8a0bf12a8244bf
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-13 14:18:59 +0530

    Time: 5 ms (48.55%) | Memory: 11.1 MB (73.80%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,55 +1,28 @@
-// class Solution {
-// public:
-//     string frequencySort(string s) {
-//         int n = s.size();
-//         string ans;
-//         unordered_map<char, int> mp;
-//         for (auto x : s) {
-//             mp[x]++;
-//         }
-//         priority_queue<pair<int, char>> pq;
-//         for (auto& x : mp) {
-//             pq.push(make_pair(x.second, x.first));
-//         }
-//         while (!pq.empty()) {
-//             int freq = pq.top().first;
-//             char ch = pq.top().second;
-//             pq.pop();
-//             for (int i = 0; i < freq; i++) {
-//                 ans.push_back(ch);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
 class Solution {
 public:
     string frequencySort(string s) {
         int n = s.size();
-        // string s;
-        unordered_map<char, int>mp;
-        for(auto x : s){
+        string ans;
+        unordered_map<char, int> mp;
+        for (auto x : s) {
             mp[x]++;
         }
-        priority_queue<pair<int, char> >pq;
-        for(auto & x:mp){
+        priority_queue<pair<int, char>> pq;
+        for (auto& x : mp) {
             pq.push(make_pair(x.second, x.first));
         }
-
-        string ans;
-        while(!pq.empty()){
+        while (!pq.empty()) {
             int freq = pq.top().first;
-            int ch = pq.top().second;
+            char ch = pq.top().second;
             pq.pop();
-
-            for(int i =0; i<freq; i++){
+            for (int i = 0; i < freq; i++) {
                 ans.push_back(ch);
             }
         }
         return ans;
     }
 };
+
+
+
+
