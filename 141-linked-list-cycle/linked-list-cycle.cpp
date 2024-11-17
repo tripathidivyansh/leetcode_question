be8029ef9b0ad467e17859a1a043e1f6552c9554
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-17 14:54:15 +0530

    Time: 7 ms (84.63%) | Memory: 11 MB (21.31%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,43 +6,43 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//         if(head == nullptr || head->next == nullptr){
-//             return false;
-//         }
-//         ListNode* slow = head;
-//         ListNode* fast = head->next;
-
-//         while(fast != slow){
-//             if(fast-> next == NULL || fast-> next-> next == NULL){
-//                 return false;
-//             }
-//             slow = slow-> next;
-//             fast = fast->next->next;
-//         }
-//         return true;
-//     }
-// };
-
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL){
+        if(head == nullptr || head->next == nullptr){
             return false;
         }
-        ListNode* fast = head;
         ListNode* slow = head;
+        ListNode* fast = head->next;
 
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast = fast->next->next;
-            if(slow == fast){
-                return true;
+        while(fast != slow){
+            if(fast-> next == NULL || fast-> next-> next == NULL){
+                return false;
             }
+            slow = slow-> next;
+            fast = fast->next->next;
         }
-        return false;
+        return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head == NULL){
+//             return false;
+//         }
+//         ListNode* fast = head;
+//         ListNode* slow = head;
+
+//         while(fast != NULL && fast->next != NULL){
+//             slow = slow->next;
+//             fast = fast = fast->next->next;
+//             if(slow == fast){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
