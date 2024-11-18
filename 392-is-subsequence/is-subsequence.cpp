9022600bc0c00b518b807f2f76cc490d031001ef
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-11-18 17:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (68.61%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,34 +1,35 @@
-// class Solution {
-// public:
-//     bool isSubsequence(string s, string t) {
-//         int n = s.size();
-//         int j = t.size();
-//         int count = 0;
-//         for(int i = 0; i<j; i++){
-//             if(s[i] == t[i]){
-//                 count++;
-//             }
-//         }
-//         if(count == s.size()){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sindex = 0;
-        int tindex = 0;
-
-        while(sindex < s.size() && tindex < t.size()){
-            if(s[sindex]== t[tindex]){
-                sindex++;
+        int n = s.size();
+        int j = t.size();
+        int count = 0;
+        for (int i = 0; i < j && count < n; i++) { 
+            if (s[count] == t[i]) {  
+                count++;
             }
-            tindex++;
         }
-        return sindex == s.size();
+        if(count == n){
+            return true;
+        }
+        return false;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int sindex = 0;
+//         int tindex = 0;
+
+//         while(sindex < s.size() && tindex < t.size()){
+//             if(s[sindex]== t[tindex]){
+//                 sindex++;
+//             }
+//             tindex++;
+//         }
+//         return sindex == s.size();
+//     }
+// };
