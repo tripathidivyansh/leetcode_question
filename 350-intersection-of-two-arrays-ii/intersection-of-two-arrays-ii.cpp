5cd1f3ccac5411844598c8e836d38001e15562a0
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-02 22:35:12 +0530

    Time: 3 ms (32.96%) | Memory: 14.9 MB (7.53%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -4,17 +4,45 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
+        unordered_map<int,int>freq;
         vector<int>result;
-        vector<bool> visited(m, false);
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if (!visited[j] && nums1[i] == nums2[j]){
-                    result.push_back(nums1[i]);
-                    visited[j] = true;
-                    break;
-                }
+
+        for(int num : nums1){
+            freq[num]++;
+        }
+        for(int num : nums2){
+            if(freq[num] > 0){
+                result.push_back(num);
+                freq[num]--;
             }
         }
         return result;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+//         int n = nums1.size();
+//         int m = nums2.size();
+
+//         vector<int>result;
+//         vector<bool> visited(m, false);
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<m; j++){
+//                 if (!visited[j] && nums1[i] == nums2[j]){
+//                     result.push_back(nums1[i]);
+//                     visited[j] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
