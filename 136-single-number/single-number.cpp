93eb559e02599b39cf59313a75f1b089c1f89e9e
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-05 11:44:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (30.18%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,28 +1,28 @@
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-//         int n = nums.size();
-//         int unique = 0;
-//         for(int i = 0; i<n; i++){
-//             unique ^= nums[i];
-//         }
-//         return unique;
-//     }
-// };
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mp;
+        int unique = 0;
         for(int i = 0; i<n; i++){
-            mp[nums[i]]++;
+            unique ^= nums[i];
         }
-        for(auto &num : mp){
-            if(num.second == 1){
-                return num.first;
-            }
-        }
-        return -1;
+        return unique;
     }
 };
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int>mp;
+//         for(int i = 0; i<n; i++){
+//             mp[nums[i]]++;
+//         }
+//         for(auto &num : mp){
+//             if(num.second == 1){
+//                 return num.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
