cb5eaed13c78859eb1f5004dd6498fd52788a3a1
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-06 21:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (24.46%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,17 +1,33 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int sum = 0;
+//         for(int i = 0; i<=n; i++){
+//             sum += i;
+//         }
+//         int leftSum = 0;
+//         for(int i = 0; i<=n; i++){
+//             if(leftSum == sum-leftSum-i){
+//                 return i;
+//             }
+//             leftSum += i;
+//         }
+//         return -1;
+//     }
+// };
+
+
 class Solution {
 public:
     int pivotInteger(int n) {
-        int sum = 0;
-        for(int i = 0; i<=n; i++){
-            sum += i;
-        }
+        int totalSum = (n * (n + 1)) / 2; 
         int leftSum = 0;
-        for(int i = 0; i<=n; i++){
-            if(leftSum == sum-leftSum-i){
-                return i;
+        for (int i = 1; i <= n; i++) {
+            if (2 * leftSum + i == totalSum) {
+                return i; 
             }
-            leftSum += i;
+            leftSum += i; 
         }
-        return -1;
+        return -1; 
     }
 };
