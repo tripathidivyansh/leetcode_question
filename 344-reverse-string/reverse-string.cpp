ab32d525e28271a3950c1f794132b79239fb1707
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-06 16:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (7.28%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,27 @@
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int n = s.size();
+//         stack<char>s1;
+//         for(int i = 0; i<n; i++){
+//             s1.push(s[i]);
+//         }
+//         for(int i = 0; i<n; i++){
+//             return s1.top();
+//             s1.pop();
+//         }
+        
+//     }
+// };
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int left = 0, right = s.size()-1;
+       while(left < right){
+        swap(s[left], s[right]);
+        left++;
+        right--;
+       }
+    }
+};
