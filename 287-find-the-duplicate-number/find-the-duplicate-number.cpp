7815833516793af7554b3e83346c90bda9316b8d
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-07 01:09:53 +0530

    Time: 23 ms (42.07%) | Memory: 65.1 MB (27.39%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,17 +1,41 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int>mp;
+//         for(int i = 0; i<n; i++){
+//             mp[nums[i]]++;
+//         }
+//         int count = 1;
+//         for(auto num : mp){
+//             if(num.second > 1){
+//                 return num.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>mp;
-        for(int i = 0; i<n; i++){
-            mp[nums[i]]++;
-        }
-        int count = 1;
-        for(auto num : mp){
-            if(num.second > 1){
-                return num.first;
+        int low = 0, high = nums.size()-1;
+
+        while(low < high){
+            int count = 0;
+           int mid = (low+high)/2;
+           for(int num : nums){
+            if(num <= mid){
+                count++;
             }
+           } 
+           if(count > mid){
+            high = mid;
+           }else{
+            low = mid+1;
+           }
         }
-        return -1;
+        return low;
     }
 };
