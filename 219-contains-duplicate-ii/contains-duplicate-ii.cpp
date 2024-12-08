19ebcab45f3232a41c10b20fa62f58c819f2efc7
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-08 09:58:15 +0530

    Time: 1584 ms (11.67%) | Memory: 64 MB (87.25%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        bool found = false;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n && (j-i) <= k; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n && (j - i) <= k; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
