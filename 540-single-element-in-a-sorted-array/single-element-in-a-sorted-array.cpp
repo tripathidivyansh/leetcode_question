ec1eee63ed08110e57b769f7772bd235860693e2
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-27 20:32:46 +0530

    Time: 15 ms (4.90%) | Memory: 31.2 MB (6.59%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        // if(nums.size() == 1){
+        //     return nums[0];
+        // }
+
+        // if(nums[0] != nums[1]){
+        //     return nums[0];
+        // }
+
+        // if(nums[nums.size() - 1] != nums[nums.size() - 2]){
+        //     return nums[nums.size() - 1];
+        // }
+
+        // int low = 1,high = nums.size() - 2;
+        // while(low <= high){
+        //     int mid = (low + high)/2;
+
+        //     if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid-1]){
+        //         return nums[mid];
+        //     }
+            
+        //     if((mid % 2 == 1 && nums[mid-1] == nums[mid]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]){
+        //         low = mid + 1;
+        //     }
+        //     else{
+        //         high = mid - 1;
+        //     }
+        // }
+        // return -1;
+
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for(int i = 0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        for(auto &num : mp){
+            if(num.second == 1){
+                return num.first;
+            }
+        }
+        return -1;
+    }
+};
