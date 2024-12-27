4c6f1e377365d19401c317b5f26fd431488f1bf7
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-27 20:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (14.80%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return (a+b);
+        // return (a+b);
+        while(b!= 0){
+            int carray = (a&b) << 1;
+            a = a^b;
+            b = carray;
+        }
+        return a;
     }
 };
