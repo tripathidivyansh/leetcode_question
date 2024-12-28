3de4161416c819b83c86ae0bf44eba9a109d0224
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2024-12-28 20:06:29 +0530

    Time: 4 ms (50.04%) | Memory: 8.9 MB (40.40%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        int ans = 1;
-        for(int i = 0; i<31; i++){
-            if(ans == n) return true;
-            if(ans > INT_MAX /3) break;
-            ans *= 3;
-        }
-        return false;
+    bool isPowerOfThree(int n,  int ans = 1) {
+        // int ans = 1;
+        // for(int i = 0; i<31; i++){
+        //     if(ans == n) return true;
+        //     if(ans > INT_MAX /3) break;
+        //     ans *= 3;
+        // }
+        // return false;
+        if (ans == n) return true; 
+        if (ans > INT_MAX / 3 || ans > n) return false; 
+        return isPowerOfThree(n, ans * 3); 
     }
 };
