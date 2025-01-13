30e3af05fd91bcc2e6375c9d0b28c437cab3336b
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-13 18:56:44 +0530

    Time: 3 ms (78.07%) | Memory: 23.5 MB (67.25%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>ans;
-        int maxi = INT_MIN;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(nums[i] == -nums[j]){
-                    maxi = max(maxi, nums[i]);
-                }
+        // int n = nums.size();
+        // int maxi = INT_MIN;
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(nums[i] == -nums[j]){
+        //             maxi = max(maxi, nums[i]);
+        //         }
+        //     }
+        // }
+        // if(maxi == INT_MIN){
+        //     return -1;
+        // }
+        // return maxi;
+        sort(nums.begin(), nums.end());
+        int left = 0, right = nums.size()-1;
+        int maxi = -1;
+
+        while(left < right){
+            if(nums[right] == - nums[left]){
+                maxi = max(maxi, nums[right]);
+                left++;
+                right--;
+            }else if(nums[right] > -nums[left]){
+                right--;
+            }else{
+                left++;
             }
-        }
-        if(maxi == INT_MIN){
-            return -1;
         }
         return maxi;
     }
