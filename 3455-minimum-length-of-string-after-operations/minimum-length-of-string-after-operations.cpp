d72499997c76cc31c1f516550019a301efced54f
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-13 22:19:00 +0530

    Time: 55 ms (39.11%) | Memory: 32.6 MB (36.69%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        unordered_map<char,int>mp;
+        int ans = 0;
+        for(int i = 0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        for(auto malik:mp){
+            int count = malik.second;
+            while(count >= 3){
+                count -= 2;
+            }
+            ans += count;
+        }
+        return ans;
+    }
+};
