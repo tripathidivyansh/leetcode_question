18fb1c1d06d672215409f986284240315123d49a
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-14 17:08:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (32.73%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         int index = 0;
         for(int i = 0; i<n; i++){
-            // if(nums[i] != val){
-            //     nums[index] = nums[i];
-            //     index++;
-            // }
-            if(nums[i] == val){
-
-            }else{
+            if(nums[i] != val){
                 nums[index] = nums[i];
                 index++;
             }
+            // if(nums[i] == val){
+
+            // }else{
+            //     nums[index] = nums[i];
+            //     index++;
+            // }
         }
         return index; 
     }
