c5753ec7084887404a949a661a0c0497a30a9625
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-16 14:59:52 +0530

    Time: 9 ms (71.87%) | Memory: 28.7 MB (35.82%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -4,6 +4,21 @@ public:
         int n = names.size();
         int m = heights.size();
 
+        // vector<pair<int, string>>people;
+        // for(int i = 0; i<n; i++){
+        //     people.push_back({heights[i], names[i]});
+        // }
+        // sort(people.begin(), people.end(), [](pair<int,string>&a, pair<int,string>&b){
+        //     return a.first > b.first;
+        // });
+
+        // vector<string>result;
+        // for(auto &malik : people){
+        //     result.push_back(malik.second);
+        // }
+        // return result;
+
+
         vector<pair<int, string>>people;
         for(int i = 0; i<n; i++){
             people.push_back({heights[i], names[i]});
@@ -11,9 +26,8 @@ public:
         sort(people.begin(), people.end(), [](pair<int,string>&a, pair<int,string>&b){
             return a.first > b.first;
         });
-
         vector<string>result;
-        for(auto &malik : people){
+        for(auto malik : people){
             result.push_back(malik.second);
         }
         return result;
