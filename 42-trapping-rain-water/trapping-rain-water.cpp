81b44111544bc921feb16fede3f74d5e58ad4e9b
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-19 23:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (86.64%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,31 +2,149 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n==0) return 0;
-        int leftmax = 0, rightmax =0;
-        int left = 0, right = n-1;
+        if(n == 0) return 0;
         int water = 0;
-        while(left <=right){
+        int left = 0, right = height.size()-1;
+        int leftmax =0 , rightmax = 0;
+
+        while(left<=right){
             if(height[left] <height[right]){
                 if(height[left] > leftmax){
                     leftmax = height[left];
                 }else{
-                    water += leftmax-height[left];
+                    water += leftmax- height[left];
                 }
                 left++;
             }else{
                 if(height[right] >= rightmax){
                     rightmax = height[right];
                 }else{
-                    water += rightmax-height[right];
+                    water += rightmax - height[right];
                 }
                 right--;
             }
-
         }
         return water;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         if(n==0) return 0;
+//         int leftmax = 0, rightmax =0;
+//         int left = 0, right = n-1;
+//         int water = 0;
+//         while(left <=right){
+//             if(height[left] <height[right]){
+//                 if(height[left] > leftmax){
+//                     leftmax = height[left];
+//                 }else{
+//                     water += leftmax-height[left];
+//                 }
+//                 left++;
+//             }else{
+//                 if(height[right] >= rightmax){
+//                     rightmax = height[right];
+//                 }else{
+//                     water += rightmax-height[right];
+//                 }
+//                 right--;
+//             }
+
+//         }
+//         return water;
+//     }
+// };
 
 
 // class Solution {
