f15caf7b76d9bdf354527c02ae2be644f1bb4eaf
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-23 22:19:01 +0530

    Time: 35 ms (91.03%) | Memory: 62.5 MB (63.61%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void func(int start, int n, int k, vector<int>& subset, vector<vector<int>>& result) {
-        if (subset.size() == k) {
-            result.push_back(subset);
+    void findCombination(int start, int n, int k, vector<vector<int>>& ans, vector<int>& ds) {
+        if (ds.size() == k) {
+            ans.push_back(ds);
             return;
         }
         for (int i = start; i <= n; i++) {
-            subset.push_back(i);
-            func(i + 1, n, k, subset, result);
-            subset.pop_back();
+            ds.push_back(i);
+            findCombination(i + 1, n, k, ans, ds);
+            ds.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int> subset;
-        vector<vector<int>> result;
-        func(1, n, k, subset, result);
-        return result;
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(1, n, k, ans, ds);
+        return ans;
     }
 };
