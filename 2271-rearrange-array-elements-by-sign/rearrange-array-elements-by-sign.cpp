74a082101dd74fe799043224699abd02aba70707
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-27 21:40:40 +0530

    Time: 11 ms (33.53%) | Memory: 134.3 MB (37.33%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         int i = 0, j = 0;
         vector<int>v;
-        while(i < n && j <n){
-            while(i < n && nums[i] < 0){
-               i++;
+        while(i < n && j < n){
+            while(i<n && nums[i] < 0){
+                i++;
             }
             if(i < n){
                 v.push_back(nums[i]);
@@ -15,7 +15,7 @@ public:
             while(j < n && nums[j] >= 0){
                 j++;
             }
-            if(j<n){
+            if(j < n){
                 v.push_back(nums[j]);
                 j++;
             }
