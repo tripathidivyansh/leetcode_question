76dd443c93700dd01bf97a50a7aadd56afe41f13
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-27 16:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (84.33%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,13 +4,11 @@ public:
         int n = nums.size();
         int count = 0;
         int ans = -1;
-
         for(int i = 0; i<n; i++){
             if(count == 0){
-            ans = nums[i];
-            count++;
-            }
-            else if(nums[i] == ans){
+                ans = nums[i];
+                count++;
+            }else if(nums[i] == ans){
                 count++;
             }else{
                 count--;
