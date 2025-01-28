c784a4dec1ffaa4104ebb68e45cae6fb64cb5712
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-28 20:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (52.58%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -14,16 +14,17 @@ public:
                         matrix[k][j] = -999;
                     }
                     for(int a = 0; a<m; a++){
-                        if(matrix[i][a] == 0){
-                            continue;
-                        }
-                        matrix[i][a] = -999;
-                    }
+                if(matrix[i][a] == 0){
+                    continue;
                 }
-            }     
+                matrix[i][a] = -999;
+            }
+                }
+            }
+            
         }
-        for(int i =0 ; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
                 if(matrix[i][j] == -999){
                     matrix[i][j] = 0;
                 }
