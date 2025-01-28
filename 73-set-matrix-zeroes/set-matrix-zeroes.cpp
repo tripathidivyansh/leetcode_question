328bb4c38d7a3d71202e83ad7048e96e36d850a4
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-01-28 23:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (25.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,93 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
         int n = matrix.size();
         int m = matrix[0].size();
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == 0){
-                    for(int k = 0; k<n; k++){
-                        if(matrix[k][j] == 0){
-                            continue;
-                        }
-                        matrix[k][j] = -999;
-                    }
-                    for(int a = 0; a<m; a++){
-                        if(matrix[i][a] == 0){
-                            continue;
-                        }
-                        matrix[i][a] = -999;
-                    }
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == -999){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 };
-
-
-// class Solution {
-// public:
-//     void setZeroes(vector<vector<int>>& matrix) {
-//         int n = matrix.size();
-//         int m = matrix[0].size();
-
-//         for(int i = 0; i<n; i++){
-//             for(int j = 0; j<m; j++){
-//                 if(matrix[i][j] == 0){
-//                     for(int k = 0; k<n; k++){
-//                         if(matrix[k][j] == 0){
-//                             continue;
-//                         }
-//                         matrix[k][j] = -999;
-//                     }
-//                     for(int a = 0; a<m; a++){
-//                         if(matrix[i][a] == 0){
-//                             continue;
-//                         }
-//                         matrix[i][a] = -999;
-//                     }
-//                 }
-//             }
-//         }
-//         for(int i = 0; i<n; i++){
-//             for(int j = 0; j<m; j++){
-//                 if(matrix[i][j] == -999){
-//                     matrix[i][j] = 0;
-//                 }
-//             }
-//         }
-//     }
-// };
