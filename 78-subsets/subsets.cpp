479d85ee3b90c6d8bfe80cdf5d530bb4ac0f0cfd
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-02-05 02:11:07 +0530

    Time: 4 ms (13.42%) | Memory: 17 MB (5.12%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,35 +1,21 @@
 class Solution {
+private:
+    void solve(vector<int>nums, vector<int>output, int index, vector<vector<int>>&ans){
+        if(index >= nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        solve(nums, output, index+1, ans);
+        int element = nums[index];
+        output.push_back(element);
+        solve(nums, output, index+1, ans);
+    }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        int n = nums.size();
-        for(int i = 0; i<(1<<n); i++){
-            vector<int>sub;
-            for(int j =0; j<n; j++){
-                if(i & (1<<j))sub.push_back(nums[j]);
-            }
-            ans.push_back(sub);
-        }
+        vector<int>output;
+        int index = 0;
+        solve(nums, output, index, ans);
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     void helper(int i, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
-//         if (i == nums.size()) {
-//             ans.push_back(current);
-//             return;
-//         }
-//         helper(i + 1, nums, current, ans);
-//         current.push_back(nums[i]);
-//         helper(i + 1, nums, current, ans);
-//         current.pop_back();
-//     }
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>> ans;
-//         vector<int> current;
-//         helper(0, nums, current, ans);
-//         return ans;
-//     }
-// };
