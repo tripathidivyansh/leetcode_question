2034555c9edb48db7dc5fd6378426294acf9d592
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-02-05 20:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.40%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,8 +1,25 @@
 class Solution {
+private: 
+    double mypow1(double x, int n){
+        if(n==0){
+            return 1;
+        }
+        if(x==1){
+            return 1;
+        }
+        double half = mypow1(x, n/2);
+        if(n%2 == 0){
+            return half*half;
+        }else{
+            return (n > 0) ? x * half * half : (half * half) / x;
+        }
+    }
 public:
     double myPow(double x, int n) {
         // double ans = pow(x, n);
         // return ans;
-        return pow(x,n);
+        // return pow(x,n);
+
+        return mypow1(x,n);
     }
 };
