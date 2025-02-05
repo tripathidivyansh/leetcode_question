8ffef13b69ea052eecce249ef7df3de36cb426ca
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-02-05 19:55:38 +0530

    Time: 195 ms (15.57%) | Memory: 90 MB (8.18%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        
+
         int maxi = 1;
         if(n==0){
             return 0;
@@ -23,29 +23,3 @@ public:
         return maxi;
     }   
 };
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         int n = nums.size();
-        
-//         int maxi = 1;
-//         if(n==0){
-//             return 0;
-//         }
-//         set<int>s;
-//         for(int i = 0; i<n; i++){
-//             s.insert(nums[i]);
-//         }
-//         for(auto malik : s){
-//             if(s.find(malik-1) == s.end()){
-//                 int count = 0;
-//                 while(s.find(malik++) != s.end()){
-//                     count++;
-//                 }
-//                 maxi = max(maxi, count);
-//             }
-
-//         }
-//         return maxi;
-//     }   
-// };
