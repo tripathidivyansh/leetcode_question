e5a7d73b30843d4cda8af01ed3e17b39f68f06d3
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-03-02 00:00:17 +0530

    Time: 79 ms (47.70%) | Memory: 11 MB (82.14%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int solve(int start, int end, vector<vector<int>>&dp){
-        if(start>=end){
+        if(start >= end){
             return 0;
         }
-        if(dp[start][end]  != -1){
+        if(dp[start][end] != - 1){
             return dp[start][end];
         }
         int maxi = INT_MAX;
         for(int i = start; i<end; i++){
             maxi = min(maxi, i+max(solve(start, i-1, dp), solve(i+1, end, dp)));
-            // maxi = min(maxi, i+max(solve(start, i-1, dp), solve(i+1, end, dp)));
         }
         dp[start][end] = maxi;
         return dp[start][end];
