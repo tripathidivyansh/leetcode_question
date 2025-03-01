44cbcebe6d1c6022198bee0a437da4c97c3fa32b
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-03-01 22:53:46 +0530

    Time: 42 ms (91.31%) | Memory: 18.7 MB (67.68%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,35 +1,17 @@
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int row = matrix.size();
-//         int column = matrix[0].size();
-
-//         int r = 0;
-//         int c = column-1;
-//         while(r < row && c >= 0){
-//             if(matrix[r][c] == target){
-//                 return true;
-//             }else if(matrix[r][c] > target){
-//                 c--;
-//             }else{
-//                 r++;
-//             }
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int column = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        for(int i = 0; i<row; i++){
-            for(int j = 0; j<column; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int i = 0, j = m-1;
+        while(i<n && j>=0){
+            if(matrix[i][j] == target){
+                return true;
+            }else if(matrix[i][j] < target){
+                i++;
+            }else{
+                j--;
             }
         }
         return false;
