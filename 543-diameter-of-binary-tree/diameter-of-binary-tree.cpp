e49dca360206f07d51ac6d9bf139aa4ca870cafa
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-03-20 15:55:13 +0530

    Time: 2 ms (27.29%) | Memory: 23.8 MB (42.64%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -9,44 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// private:
-//     int height(TreeNode* node, int &diameterOfBinaryTree){
-//         if(node == NULL){
-//             return 0;
-//         }
-//         int leftHeight = height(node->left, diameterOfBinaryTree);
-//         int rightHeight = height(node->right, diameterOfBinaryTree);
-
-//         diameterOfBinaryTree = max(diameterOfBinaryTree, leftHeight + rightHeight);
-//         return  max(leftHeight, rightHeight) + 1;
-//     }
-// public:
-//     int diameterOfBinaryTree(TreeNode* root) {
-//         int diameterOfBinaryTree = 0;
-//         height(root, diameterOfBinaryTree);
-
-//         return diameterOfBinaryTree;
-//     }
-// };
-
 class Solution {
 public:
+    int solve(TreeNode* root,int&maxi){
+        if(root == NULL){
+            return 0;
+        }
+        int left = solve(root->left, maxi);
+        int right = solve(root->right, maxi);
+        maxi = max(maxi, left+right);
+
+        return 1 + max(left, right);
+    }
     int diameterOfBinaryTree(TreeNode* root) {
         int maxi = 0;
-        findmax(root, maxi);
+        solve(root, maxi);
         return maxi;
-    }
-
-private:
-    int findmax(TreeNode* node, int& maxi) {
-        if (node == NULL) return 0;
-
-        int leftheight = findmax(node->left, maxi);
-        int rightheight = findmax(node->right, maxi);
-
-        maxi = max(maxi, leftheight + rightheight);
-
-        return 1 + max(leftheight, rightheight);
     }
 };
