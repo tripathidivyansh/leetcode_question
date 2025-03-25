57cf2a16f25541c454b303a26d9662d8290ace2e
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-03-25 14:50:49 +0530

    Time: 30 ms (20.54%) | Memory: 12.1 MB (52.49%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    void solve(int index, vector<int>& nums, set<vector<int>>& result) {
-        if (index >= nums.size()) {
+
+    void solve(int index, vector<int>&nums, set<vector<int>>&result){
+        if(index >= nums.size()){
             result.insert(nums);
             return;
         }
-        for (int i = index; i < nums.size(); i++) {
+        for(int i = index; i<nums.size(); i++){
             swap(nums[index], nums[i]);
-            solve(index + 1, nums, result);
+            solve(index+1, nums, result);
             swap(nums[index], nums[i]);
         }
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> result;
+        set<vector<int>>result;
         solve(0, nums, result);
+
         return vector<vector<int>>(result.begin(), result.end());
     }
 };
