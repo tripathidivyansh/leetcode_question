fea6ce366916595a0156856704c21a49f1987bca
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-03-28 22:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.75%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,23 +1,21 @@
 class Solution {
-private: 
-    double mypow1(double x, int n){
+public:
+
+    double solve(double x, long n){
         if(n==0){
             return 1;
         }
-        if(x==1){
-            return 1;
+        if(n < 0){
+            return 1 / solve(x, -n);
         }
-        double half = mypow1(x, n/2);
+        double half = solve(x, n/2);
         if(n%2 == 0){
             return half*half;
-        }else if(n>0){
-            return  x * half * half;
         }else{
-            return (half * half) / x;
+            return half*half*x;
         }
     }
-public:
     double myPow(double x, int n) {
-        return mypow1(x,n);
+        return solve(x, long(n));
     }
 };
