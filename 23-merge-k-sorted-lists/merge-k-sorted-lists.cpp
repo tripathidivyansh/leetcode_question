72b435ac074b05fd039de09eb87f247882f12ddc
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-04-26 21:36:48 +0530

    Time: 3 ms (63.69%) | Memory: 18.5 MB (50.38%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,38 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    // Helper to merge two lists
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode dummy;
-        ListNode* tail = &dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                tail->next = l1;
-                l1 = l1->next;
-            } else {
-                tail->next = l2;
-                l2 = l2->next;
-            }
-            tail = tail->next;
-        }
-        if (l1) tail->next = l1;
-        if (l2) tail->next = l2;
-        return dummy.next;
-    }
-    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
-        
-        ListNode* head = lists[0];
-        for (int i = 1; i < lists.size(); i++) {
-            head = merge(head, lists[i]);  
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists[i]) {
+                pq.push({lists[i]->val, lists[i]});
+            }
         }
-        return head;
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+
+        while (!pq.empty()) {
+            pair<int, ListNode*> p = pq.top();
+            pq.pop();
+
+            ListNode* node = p.second;
+            temp->next = node;
+            temp = temp->next;
+
+            if (node->next) {
+                pq.push({node->next->val, node->next});
+            }
+        }
+        return dummy->next;
     }
 };
