c344d0919ef276bbea6a0778d9e73410cb1a2a6e
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-04-26 20:48:44 +0530

    Time: 95 ms (17.11%) | Memory: 18.9 MB (30.09%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,23 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> arr;
-        for (int i = 0; i < lists.size(); i++) {
-            ListNode* temp = lists[i];
-            while (temp != NULL) {
-                arr.push_back(temp->val); 
-                temp = temp->next;        
+    // Helper to merge two lists
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
             }
+            tail = tail->next;
         }
-        sort(arr.begin(), arr.end());
-
-        ListNode* dummy = new ListNode(0);
-        ListNode* current = dummy;
-
-        for (int val : arr) {
-            current->next = new ListNode(val);
-            current = current->next;
+        if (l1) tail->next = l1;
+        if (l2) tail->next = l2;
+        return dummy.next;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        
+        ListNode* head = lists[0];
+        for (int i = 1; i < lists.size(); i++) {
+            head = merge(head, lists[i]);  
         }
-        return dummy->next;
+        return head;
     }
 };
