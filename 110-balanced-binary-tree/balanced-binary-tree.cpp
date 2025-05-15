799002af6eb481c232af0dc0966e0976efc7a6fe
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-05-15 10:11:02 +0530

    Time: 67 ms (5.86%) | Memory: 23.3 MB (6.44%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,50 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-// class Solution {
-// private:
-//     int height(struct TreeNode* node) {
-//         if (node == NULL) {
-//             return 0;
-//         }
-//         int left = height(node->left);
-//         int right = height(node->right);
-//         return max(left, right) + 1;
-//     }
-// public:
-//     bool isBalanced(TreeNode* root) {
-//         if (root == NULL) {
-//             return true;
-//         }
-//         bool leftBalanced = isBalanced(root->left);
-//         bool rightBalanced = isBalanced(root->right);
-//         bool heightDiff = abs(height(root->left) - height(root->right)) <= 1;
-//         return leftBalanced && rightBalanced && heightDiff;
-//     }
-// };
-
 class Solution {
-private:
-    int dfsHeight(TreeNode* node) {
-        if (node == NULL) {
+public:
+
+    int height(TreeNode* root){
+        if(root == NULL){
             return 0;
         }
+        int left = height(root->left);
+        int right = height(root->right);
 
-        int leftHeight = dfsHeight(node->left);
-        if (leftHeight == -1) return -1;
-
-        int rightHeight = dfsHeight(node->right);
-        if (rightHeight == -1) return -1;
-
-        if (abs(leftHeight - rightHeight) > 1) return -1;
-
-        return max(leftHeight, rightHeight) + 1;
+        int ans = max(left, right) + 1;
+        return ans;
     }
-
-public:
     bool isBalanced(TreeNode* root) {
-        return dfsHeight(root) != -1;
+        if(root == NULL){
+            return true;
+        }
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+
+        bool diff = abs(height(root->left) - height(root->right)) <= 1;
+
+        if(left && right && diff){
+            return true;
+        }
+        return false;
     }
 };
