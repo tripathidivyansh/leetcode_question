5ebb98289184aad69c09ccd7942f14b71ebc9796
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-05-26 15:20:04 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (62.96%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,23 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-void solve(TreeNode* root, vector<int>&ans, int level){
-    if(root == NULL){
-        return;
-    }
-    if(level == ans.size()){
-        ans.push_back(root->val);
-    }
-    solve(root->right, ans, level+1);
-    solve(root->left, ans, level+1);
-}
 class Solution {
 public:
+
+    void solve(TreeNode* root, vector<int>&ans, int level){
+        if(root == NULL){
+            return;
+        }
+        if(level == ans.size()){
+            ans.push_back(root->val);
+        }
+        solve(root->right, ans, level+1);
+        solve(root->left, ans, level+1);
+    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         solve(root, ans, 0);
-        
         return ans;
     }
 };
