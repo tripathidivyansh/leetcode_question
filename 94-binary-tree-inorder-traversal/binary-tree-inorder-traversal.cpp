933dec9c4140527539f17386b0fe6b461af8af73
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-01 21:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (33.03%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,69 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         vector<int>result;
-//         inorder(root, result);
-//         return result;
-//     }
-// private:
-//     void inorder(TreeNode* node, vector<int>&result){
-//         if(node == NULL){
-//             return;
-//         }
-//         inorder(node->left, result);
-//         result.push_back(node->val);
-//         inorder(node->right, result);
-
-//     }
-// };
-
-
-
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> inorder;
-        if (root == NULL) return inorder;
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        while (!st.empty() || node != NULL) {
-            while (node != NULL) {
-                st.push(node);
-                node = node->left;
-            }
-            node = st.top();
-            st.pop();
-            inorder.push_back(node->val);
-            node = node->right;
+    void solve(TreeNode* root, vector<int>&ans){
+        if(root==NULL){
+            return;
         }
-        return inorder;
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        solve(root, ans);
+        return ans;
+
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> inorderTraversal(TreeNode* root) {
-//         stack<TreeNode*>st;
-//         TreeNode* node = root;
-//         vector<int>inorder;
-
-//         while(true){
-//             if(node != NULL){
-//                 st.push(node);
-//                 node = node->left;
-//             }else{
-//                 if(st.empty() == true)  break;
-//                 node = st.top();
-//                 st.pop();
-//                 inorder.push_back(node->val);
-//                 node = node->right;
-
-//             }
-//         }
-//         return inorder;
-//     }
-// };
-
