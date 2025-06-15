61e42058193171434d3b7c4ff57940c770776ccb
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-16 00:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (55.86%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int countDigitOne(int n) {
         long long count = 0;
-        for (long long factor = 1; factor <= n; factor *= 10) {
-            long long higher = n / (factor * 10);
-            long long current = (n / factor) % 10;
-            long long lower = n % factor;
+        for (long long i = 1; i <= n; i *= 10) {
+            long long higher = n / (i * 10);
+            long long current = (n / i) % 10;
+            long long lower = n % i;
 
             if (current == 0) {
-                count += higher * factor;
+                count += higher * i;
             } else if (current == 1) {
-                count += higher * factor + lower + 1;
+                count += higher * i + lower + 1;
             } else {
-                count += (higher + 1) * factor;
+                count += (higher + 1) * i;
             }
         }
         return count;
