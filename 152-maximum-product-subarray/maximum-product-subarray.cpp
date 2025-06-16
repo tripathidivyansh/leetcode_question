a8a32254be5a4b592206f14729e864950764b125
tripathidivyansh <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-17 02:31:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (21.18%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,8 +6,8 @@ public:
         int maxProd = nums[0];
         int minProd = nums[0];
 
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] < 0) swap(maxProd, minProd);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] <= 0) swap(maxProd, minProd);
 
             maxProd = max(nums[i], maxProd * nums[i]);
             minProd = min(nums[i], minProd * nums[i]);
