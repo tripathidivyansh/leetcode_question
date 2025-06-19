85de7c13f63d8d3905eb6065e4784e0b17a5dba3
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-20 01:23:26 +0530

    Time: 32 ms (94.91%) | Memory: 62.5 MB (62.15%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    void findCombination(int start, int n, int k, vector<vector<int>>& ans, vector<int>& ds) {
-        if (ds.size() == k) {
-            ans.push_back(ds);
+
+    void solve(int n, int k, int index, vector<vector<int>>&ans, vector<int>&path){
+        if(path.size() == k){
+            ans.push_back(path);
             return;
         }
-        for (int i = start; i <= n; i++) {
-            ds.push_back(i);
-            findCombination(i + 1, n, k, ans, ds);
-            ds.pop_back();
+        for(int i = index; i<=n; i++){
+            path.push_back(i);
+            solve(n, k, i+1, ans, path);
+            path.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findCombination(1, n, k, ans, ds);
+        vector<vector<int>>ans;
+        vector<int>path;
+
+        solve(n, k, 1, ans, path);
         return ans;
     }
 };
