6e7b16e3263e51a95668d0034d3b6daf453a916a
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-26 22:36:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (50.55%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
         int i = 0;
-        for(int j = 0; j<n; j++){
+        for(int j = 0; j<nums.size(); j++){
+            
             if(nums[j] != 0){
                 swap(nums[j], nums[i]);
                 i++;
             }
         }
-        // return nums];
     }
 };
