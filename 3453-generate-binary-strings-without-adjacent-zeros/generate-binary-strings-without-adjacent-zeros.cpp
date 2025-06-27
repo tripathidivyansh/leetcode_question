4d5c9c9a0c589059a34bbd69b8458fb6359c4613
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-27 21:10:17 +0530

    Time: 18 ms (46.30%) | Memory: 29.3 MB (45.94%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
 
-    void solve(vector<string>&ans, string temp, int n){
-        if(temp.size() == n){
+    void solve(int n, vector<string>&ans, string temp){
+        if(temp.size() >= n){
+
             ans.push_back(temp);
             return;
         }
-        solve(ans, temp + "1", n);
+        solve(n, ans, temp + "1");
         if(temp.empty() || temp.back() == '1'){
-            solve(ans, temp+"0", n);
+            solve(n, ans, temp + "0");
         }
     }
     vector<string> validStrings(int n) {
         vector<string>ans;
         string temp = "";
-        solve(ans, temp, n);
+
+        solve(n, ans, temp);
         return ans;
     }
 };
