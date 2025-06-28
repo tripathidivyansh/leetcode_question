5ad5f3a0f152e4599e802b84176d37b7fbf6af3e
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-06-28 15:10:43 +0530

    Time: 7 ms (73.80%) | Memory: 23 MB (41.47%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,76 +1,22 @@
-// class Solution {
-// public:
-
-//     int findMax(vector<int>& piles){
-//         int maxi = INT_MIN;
-//         int n = piles.size();
-//         for(int i = 0; i<n; i++){
-//             maxi = max(maxi, piles[i]);
-//         }
-//         return maxi;
-//     }
-
-//     int calculateTotalHorus(vector<int>& piles, int hourly){
-//         int totalH = 0;
-//         int n = piles.size();
-//         for(int i =0; i<n; i++){
-//             // totalH += ceil((double)piles[i] / (double)hourly);
-//             totalH += (piles[i] + hourly - 1) / hourly;
-//         }
-//         return totalH;
-//     }
-//     int minEatingSpeed(vector<int>& piles, int h) {
-//         int n = piles.size();
-//         int low = 1, high = findMax(piles);
-//         while(low<=high){
-//             int mid = (low+high)/2;
-//             int totalH =calculateTotalHorus(piles, mid);
-//             if(totalH <= h){
-//                 high = mid-1;
-//             }else{
-//                 low = mid+1;
-//             }
-//         }
-//         return low;
-//     }
-// };
-
-
 class Solution {
 public:
-    int findMax(vector<int>& piles) {
-        int maxi = INT_MIN;
-        int n = piles.size();
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, piles[i]);
-        }
-        return maxi;
-    }
-
-    long long calculateTotalHorus(vector<int>& piles, int hourly) {
-        long long totalH = 0;  // Use long long to prevent overflow
-        int n = piles.size();
-        for (int i = 0; i < n; i++) {
-            totalH += (piles[i] + hourly - 1) / hourly;  // Ceil division without using floating-point operations
-        }
-        return totalH;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1, high = findMax(piles);
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        int result = high;
 
-        while (low <= high) {
-            int mid = (low+high) / 2;
-
-            long long totalH = calculateTotalHorus(piles, mid);  // totalH now uses long long
-
-            if (totalH <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            long long  totalhourS = 0;
+            for(int pile : piles){
+                totalhourS += (pile + mid - 1)/mid;
+            }
+            if(totalhourS <= h){
+                result = mid;
+                high  = mid-1;
+            }else{
+                low = mid+1;
             }
         }
-        return low;
+        return result;
     }
 };
-
