a1b7f7f400026a7dd89608b5429cc0d88d7a0254
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-03 10:36:22 +0530

    Time: 4 ms (36.05%) | Memory: 10.5 MB (33.03%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    void solve(string &word, int k) {
+        if (word.size() >= k) {
+            return;
+            
+        }
+
+        string nextpart = "";
+        for (char c : word) {
+            if (c == 'z') {
+                nextpart += 'a';
+            } else {
+                nextpart += c + 1;
+            }
+        }
+
+
+        word += nextpart;
+        solve(word, k);
+    }
+
     char kthCharacter(int k) {
         string word = "a";
-        while(word.length() < k){
-
-            string nextpart = "";
-            for(char c : word){
-                if(c == 'z'){
-                    nextpart += 'a';
-                }else{
-                    nextpart += c+1;
-                }
-            }
-            word += nextpart;
-        }
-        
-
-        return word[k-1];
+        solve(word, k);
+        return word[k - 1];
     }
 };
