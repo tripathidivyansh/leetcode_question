db99122960a0e640b76ded4f6c23691c66ca042f
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-04 22:11:51 +0530

    Time: 3 ms (33.81%) | Memory: 9.5 MB (36.22%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.cpp
+++ 1022-unique-paths-iii/unique-paths-iii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int count = 0;
-    int totalSteps = 0;
-
-    bool solve(vector<vector<int>>& grid, int i, int j, int steps, vector<vector<bool>>& visited) {
+    bool solve(int i, int j, vector<vector<int>>& grid, int steps, int& count, int& totalStep, vector<vector<bool>>& visited) {
         int n = grid.size();
         int m = grid[0].size();
 
-    
         if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == -1 || visited[i][j]) {
             return false;
         }
-
         if (grid[i][j] == 2) {
-            if (steps == totalSteps) {
+            if (steps == totalStep) {
                 count++;
             }
             return false;
         }
 
-        visited[i][j] = true;
 
-        solve(grid, i + 1, j, steps + 1, visited);
-        solve(grid, i - 1, j, steps + 1, visited);
-        solve(grid, i, j + 1, steps + 1, visited);
-        solve(grid, i, j - 1, steps + 1, visited);
+        visited[i][j] = true;
+        solve(i + 1, j, grid, steps + 1, count, totalStep, visited);
+        solve(i - 1, j, grid, steps + 1, count, totalStep, visited);
+        solve(i, j + 1, grid, steps + 1, count, totalStep, visited);
+        solve(i, j - 1, grid, steps + 1, count, totalStep, visited);
 
         visited[i][j] = false;
 
@@ -34,21 +29,25 @@ public:
     int uniquePathsIII(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int startX, startY;
 
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int count = 0, totalStep = 0;
+
+       
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] != -1) {
-                    totalSteps++;
-                }
-                if (grid[i][j] == 1) {
-                    startX = i;
-                    startY = j;
+                    totalStep++;
                 }
             }
         }
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        solve(grid, startX, startY, 1, visited); 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    solve(i, j, grid, 1, count, totalStep, visited);
+                }
+            }
+        }
         return count;
     }
 };
