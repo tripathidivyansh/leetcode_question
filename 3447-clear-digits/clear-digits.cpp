e4bb729c09951eba95d99e67f857ce818962fcf6
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-07 20:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (67.11%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,36 +1,16 @@
-// class Solution {
-// public:
-//     string clearDigits(string s) {
-//         string ans;
-//         for(auto c : s){
-//             if(c >= '0' && c <='9')
-//                 ans.pop_back();
-//             else
-//                 ans.push_back(c);
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char>ans;
+        int n = s.size();
+        string ans;
+
         for(auto c : s){
-            if(c>= '0' && c<='9'){
-                if(!ans.empty())
-                     ans.pop();
+            if(c >= '0' && c <= '9'){
+                ans.pop_back();
             }else{
-                ans.push(c);
+                ans.push_back(c);
             }
         }
-        string ans1;
-        while(!ans.empty()){
-            ans1+= ans.top();
-            ans.pop();
-        }
-        // return ans1;
-        reverse(ans1.begin(), ans1.end());
-        return ans1;
+        return ans;
     }
 };
