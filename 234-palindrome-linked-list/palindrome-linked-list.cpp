2fcc901af1140d1cd8016fe78c1bcfa34b037cf5
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-08 11:11:23 +0530

    Time: 4 ms (53.18%) | Memory: 132 MB (16.94%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -21,7 +21,7 @@ public:
             head = head->next;
         }
         int i = 0, j = arr.size()-1;
-        while(i <j){
+        while(i <=j){
             if(arr[i] != arr[j]){
                 return false;
             }
