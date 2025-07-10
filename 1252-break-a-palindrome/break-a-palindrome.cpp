29f3faca70f1b287573846e5565377f330998aaf
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-10 11:41:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (67.90%) - LeetSync

---
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        if(palindrome.size() == 1) return "";
+        int l = 0, r = palindrome.size()-1;
+
+        for(int i = 0; i<palindrome.size()/2; i++){
+            if(palindrome[i] != 'a'){
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+
+        palindrome[palindrome.size()-1] = 'b';
+        return palindrome;
+
+
+    }
+
+};
