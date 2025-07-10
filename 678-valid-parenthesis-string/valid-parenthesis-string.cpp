2f84dc493a70d8d65d382fcb643ebcc89495c890
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-10 14:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (73.71%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -16,7 +16,9 @@ public:
                 high++;
             }
             if(high < 0) return false;
-            low = max(low, 0);
+            if(low < 0){
+                low = 0;
+            }
         }
         return low == 0;
     }
