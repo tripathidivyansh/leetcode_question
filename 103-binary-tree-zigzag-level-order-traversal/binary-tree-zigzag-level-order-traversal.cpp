2186ffc581e24ff55e48aeb8f7aeb3c8320b610b
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-14 11:19:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.66%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,42 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == NULL) {
-            return result;
-        }
+        vector<vector<int>> ans;
+        if (root == NULL) return ans;
 
         queue<TreeNode*> q;
         q.push(root);
-        bool leftToRight = true;
+        bool flag = false;
 
         while (!q.empty()) {
-            int size = q.size();
-            vector<int> level(size);
+            int Size = q.size();
+            vector<int> level;
 
-            for (int i = 0; i < size; i++) {
-                TreeNode* frontNode = q.front();
+            for (int i = 0; i < Size; i++) {
+                TreeNode* node = q.front();
                 q.pop();
+                level.push_back(node->val);
 
-                int index;
-                if (leftToRight) {
-                    index = i;
-                } else {
-                    index = size - i - 1;
+            
+                if (node->left != NULL) {
+                    q.push(node->left);
                 }
-
-                level[index] = frontNode->val;
-
-                if (frontNode->left != NULL) {
-                    q.push(frontNode->left);
-                }
-                if (frontNode->right != NULL) {
-                    q.push(frontNode->right);
+                if (node->right != NULL) {
+                    q.push(node->right);
                 }
             }
-            result.push_back(level);
-            leftToRight = !leftToRight;
+
+            if (flag) reverse(level.begin(), level.end()); 
+            ans.push_back(level);
+            flag = !flag;
         }
-        return result;
+
+        return ans;
     }
 };
