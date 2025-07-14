172072704f188fb9bf266b43ce2de7dc00ef7d8f
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-14 10:18:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (14.73%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -12,16 +12,16 @@
 class Solution {
 public:
     void solve(TreeNode* root, vector<int>&ans){
-        if(root == NULL){
-            return;
-        }
+        if(root == NULL) return;
         solve(root->left, ans);
+
         solve(root->right, ans);
         ans.push_back(root->val);
     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>ans;
         solve(root, ans);
+
         return ans;
     }
 };
