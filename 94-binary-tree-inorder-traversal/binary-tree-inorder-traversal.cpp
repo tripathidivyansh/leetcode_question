ff4a0507cde24727387097ba413e498e094c3101
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-14 09:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (31.20%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,10 +11,10 @@
  */
 class Solution {
 public:
+    
     void solve(TreeNode* root, vector<int>&ans){
-        if(root==NULL){
-            return;
-        }
+        if(root == NULL) return;
+
         solve(root->left, ans);
         ans.push_back(root->val);
         solve(root->right, ans);
@@ -22,7 +22,7 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
         solve(root, ans);
-        return ans;
 
+        return ans;
     }
 };
