934105d3f741bbe6dc3cea2595622f53c87c943d
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-16 20:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 147 MB (6.27%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -1,28 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (root == NULL) return 0;
-        queue<pair<TreeNode*, int>> q;
+        if(root == NULL) return 0;
+        queue<pair<TreeNode*, int>>q;
 
         q.push({root, 1});
-        while (!q.empty()) {
-            TreeNode* node = q.front().first;
-            int depth = q.front().second;
-            q.pop();
-            if (node->left == NULL && node->right == NULL) {
-                return depth;
-            }
-            if (node->left != NULL) {
-                q.push({node->left, depth + 1});
-            }
-            if (node->right != NULL) {
-                q.push({node->right, depth + 1});
-
+        while(!q.empty()){
+            int Size = q.size();
+            for(int i = 0; i<Size; i++){
+                TreeNode* node = q.front().first;
+                int deapth = q.front().second;
+                q.pop();
+                if(node->left == NULL && node->right == NULL){
+                    return deapth;
+                }
+                if(node->left != NULL){
+                    q.push({node->left, deapth+1});
+                }
+                if(node->right != NULL){
+                    q.push({node->right, deapth+1});
+                }
             }
         }
-        
-        return 0; 
+
+        return 0;
     }
-
 };
-
