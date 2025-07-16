aeea425930dccfce6a7b44870a359909bb7294a3
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-16 20:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 147.1 MB (6.27%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -1,27 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int solve(TreeNode* root){
-        if(root == NULL) return 0;
-        if(root->left == NULL){
-            return 1 + solve(root->right);
-        }
-        if(root->right == NULL){
-            return 1 + solve(root->left);
-        }
-        return 1 + min(solve(root->left), solve(root->right));
-    }
     int minDepth(TreeNode* root) {
-        return solve(root);
+        if (root == NULL) return 0;
+        queue<pair<TreeNode*, int>> q;
+
+        q.push({root, 1});
+        while (!q.empty()) {
+            TreeNode* node = q.front().first;
+            int depth = q.front().second;
+            q.pop();
+            if (node->left == NULL && node->right == NULL) {
+                return depth;
+            }
+            if (node->left != NULL) {
+                q.push({node->left, depth + 1});
+            }
+            if (node->right != NULL) {
+                q.push({node->right, depth + 1});
+
+            }
+        }
+        
+        return 0; 
     }
+
 };
+
