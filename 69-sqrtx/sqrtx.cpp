02513ddd585375215be16f3ad5019cf2770d3854
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-19 14:17:58 +0530

    Time: 15 ms (10.40%) | Memory: 8.7 MB (14.64%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int m = sqrt(x);
-        return m;
+        if(x <=1) return x;
+        long long ans = 0;
+        for(int i = 1; i<=x; i++){
+            long long ans1 = 1LL * i * i;
+            if(ans1 <= x){
+                ans = i;
+            }else{
+                break;
+            }
+        }
+        return ans;
     }
 };
