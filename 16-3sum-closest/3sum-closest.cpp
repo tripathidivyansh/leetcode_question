f48b7901e37337b194f119210dfae8cd9760a5ac
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-21 01:39:10 +0530

    Time: 21 ms (13.12%) | Memory: 14 MB (28.42%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,63 +1,24 @@
-// class Solution {
-// public:
-//     int threeSumClosest(vector<int>& nums, int target) {
-//         int n = nums.size();
-//         int ans = INT_MAX;
-
-//         sort(nums.begin(), nums.end());
-//         for(int i = 0; i<n-2; i++){
-//             if (i > 0 && nums[i] == nums[i - 1]) continue;
-//             int j = i+1;
-//             int k = n-1;
-
-//             while(j < k){
-//                 int sum = nums[i] + nums[j] + nums[k];
-//                 if(abs(target-sum) < abs(target-ans)){
-//                     ans = sum;
-//                 }
-//                 if(sum < target){
-//                     j++;
-//                 }else if(sum > target){
-//                     k--;
-//                 }else{
-//                     return ans;
-//                 }
-//             } 
-//         }
-//         return ans;     
-//     }
-// };
-
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
-        if (n < 3) return 0; 
-        int ans = nums[0] + nums[1] + nums[2];
-        
+        if(n < 3) return 0;
         sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (abs(sum - target) < abs(ans - target)) {
-                    ans = sum;
+        long long closetSum = INT_MAX;
+        for(int k = 0; k < n-2; k++){
+            int i = k+1, j = n-1;
+            while(i < j){
+                long long Sum = nums[i] + nums[j] + nums[k];
+                if(abs(target - Sum ) < abs(target - closetSum)){
+                    closetSum = Sum;
                 }
-
-                if (sum < target) {
-                    j++;
-                } else if (sum > target) {
-                    k--;
-                } else {
-                    return sum; 
+                if(Sum < target){
+                    i++;
+                }else{
+                    j--;
                 }
             }
         }
-        return ans;  
+        return closetSum;
     }
 };
