f4d95ce5b67e86f064bfb5cf1a27be58c64ff1b4
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-21 00:56:16 +0530

    Time: 35 ms (98.60%) | Memory: 29.6 MB (39.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n; i++) {  
-            if(i > 0 && nums[i] == nums[i - 1]) continue;  
-            
-            int j = i + 1;
-            int k = n - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;
-                } else if(sum > 0) {
-                    k--;
-                } else {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j - 1]) j++;
-                    while(j < k && nums[k] == nums[k + 1]) k--;
-                }
+    void twoSum(vector<int>&nums, int target, int i, int j, vector<vector<int>>&result){
+        while(i < j){
+            if(nums[i] + nums[j] > target){
+                j--;
+            }else if(nums[i] + nums[j] < target){
+                i++;
+            }else{
+                while(i < j && nums[i] == nums[i+1]) i++;
+                while(i < j && nums[j] == nums[j-1]) j--;
+                result.push_back({-target, nums[i], nums[j]});
+                i++;
+                j--;
             }
         }
-        return ans;
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        if(n <3) return {};
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>result;
+        for(int i = 0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int n1 = nums[i];
+            int target = -n1;
+            twoSum(nums, target, i+1, n-1, result);
+            
+        }
+        
+        return result;
     }
 };
