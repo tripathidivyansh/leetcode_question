41d0c03ec819a7d136912043023c144982152231
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-23 14:36:38 +0530

    Time: 10 ms (88.75%) | Memory: 36.6 MB (60.26%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<bool>>visited(n, vector<bool>(m, false));
+        queue<pair<int,int>>q;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(mat[i][j] == 0){
+                    q.push({i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, -1, 0, 1};
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for(int i = 0; i<4; i++){
+                int di = x + dx[i];
+                int dj = y + dy[i];
+                while(di >= 0 && dj >= 0 && di < n && dj < m && !visited[di][dj]){
+                    q.push({di, dj});
+                    visited[di][dj] = true;
+                    mat[di][dj] = mat[x][y] + 1;
+                }
+            }
+        }
+        return mat;
+    }
+};
