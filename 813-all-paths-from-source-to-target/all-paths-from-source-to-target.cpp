1deca84c41273485876ecbd7962aa65622df8584
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-24 15:29:45 +0530

    Time: 3 ms (76.62%) | Memory: 13.8 MB (76.58%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& graph, int index, vector<vector<int>>& result, vector<int>& path) {
+    void solve(int index, vector<vector<int>>&graph, vector<int>&path, vector<vector<int>>&result){
         path.push_back(index);
-        if (index >= graph.size() - 1) {
-
-            result.push_back(path);
-        } else {
-            for (int next : graph[index]) {
-                solve(graph, next, result, path);
+        if(index >= graph.size()-1){
+            result.push_back(path);   
+        }else{
+            for(int next : graph[index]){
+                solve(next, graph, path, result);
             }
         }
-
         path.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-
-        vector<vector<int>> result;
-        vector<int> path;
-        solve(graph, 0, result, path);
+        vector<int>path;
+        vector<vector<int>>result;
+        solve(0, graph, path, result);
         return result;
     }
 };
