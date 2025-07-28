0eba71e55ea29dce3d1971c2cf9776a9554347bd
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-28 13:45:24 +0530

    Time: 2 ms (7.56%) | Memory: 10.6 MB (64.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     int solve(int index, vector<int>&nums, vector<int>&dp){
         if(index >= nums.size()){
             return 0;
@@ -8,13 +7,14 @@ public:
         if(dp[index] != -1){
             return dp[index];
         }
-        int include = nums[index] + solve(index+2, nums, dp);
+        int include = nums[index] + solve(index + 2, nums, dp); 
         int exclude = solve(index+1, nums, dp);
 
         dp[index] = max(include, exclude);
         return dp[index];
     }
     int rob(vector<int>& nums) {
+        
         int n = nums.size();
         vector<int>dp(n+1, -1);
         return solve(0, nums, dp);
