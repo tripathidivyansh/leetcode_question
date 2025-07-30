cce7d4ecee16442f00987f90bbd6303e2ae8e292
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-07-31 01:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 86.1 MB (65.24%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -5,18 +5,6 @@ public:
         int count = 0;
         int maxlen = 0;
         int maxele = 0;
-        // for(int i = 0; i<n; i++){
-        //     maxele = max(maxele, nums[i]);
-        // }
-        // for(int i = 0; i<n; i++){
-        //     if(nums[i] == maxele){
-        //         count++;
-        //         maxlen = max(maxlen, count);
-        //     }else{
-        //         count = 0;
-        //     }
-        // }
-        // return maxlen;
         for(int num : nums){
             if(num > maxele){
                 maxele = num;
