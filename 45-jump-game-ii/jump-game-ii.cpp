d580900c4b3d982ebf5f3b943edf9f77e2334c67
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-08 23:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (36.51%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& nums, vector<int>& dp) {
-        int n = nums.size();
-        if (index >= n - 1) {
-            return 0;
-        }
-        if (nums[index] == 0) {
-            return 1e9;  
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        int minJumps = 1e9;
-        for (int i = 1; i <= nums[index]; i++) {
-            if (index + i < n) {  
-                int jumpsFromNext = solve(index + i, nums, dp);
-                if (jumpsFromNext < minJumps) {
-                    minJumps = jumpsFromNext;
-                }
-            }
-        }
-        dp[index] = 1 + minJumps;
-        return dp[index];
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1); 
-        int result = solve(0, nums, dp);
-
-        if (result >= 1e9) {
-            return -1;
-        } else {
-            return result;
+        if(n<= 1){
+            return 0;
         }
+        int jumpS = 0, curr = 0,next = 0;
+        for(int i = 0; i<n-1; i++){
+            next = max(next, i+nums[i]);
+
+            if(i == curr){
+                jumpS++;
+                curr = next;
+            }
+        }
+        return jumpS;
     }
 };
