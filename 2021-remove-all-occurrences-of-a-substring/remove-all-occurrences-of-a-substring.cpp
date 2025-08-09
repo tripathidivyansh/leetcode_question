2df68184409244b8bc933e1c497af6b25832c66d
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-09 23:00:14 +0530

    Time: 3 ms (33.58%) | Memory: 12.1 MB (10.14%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(true){
-            int idx = s.find(part);
+        // while(true){
+        //     int idx = s.find(part);
             
-            if(idx == string::npos){
-                break;
-            }
+        //     if(idx == string::npos){
+        //         break;
+        //     }
 
-            s.erase(idx, part.length());
+        //     s.erase(idx, part.length());
+        // }
+        // return s;
+        string result = "";
+        int n = part.size();
+        for(char &ch : s){
+            result.push_back(ch);
+            if(result.length() >= n && result.substr(result.length() - n) == part){
+                result.erase(result.length() -n);
+            }
         }
-        return s;
+        return result;
     }
 
 
