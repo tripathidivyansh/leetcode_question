9d70cd345421ccc5a2e12c0c1836ea7cc09518ea
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-09 15:13:35 +0530

    Time: 158 ms (68.37%) | Memory: 195.1 MB (53.30%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -9,71 +9,79 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* mapParents(TreeNode* root, int start, unordered_map<TreeNode*, TreeNode*>& parentMap) {
-        TreeNode* targetNode = nullptr;
+    int amountOfTime(TreeNode* root, int start) {
+        if (root == nullptr) return -1;
+
+        unordered_map<TreeNode*, TreeNode*> par; 
+        par[root] = nullptr;
+
         queue<TreeNode*> q;
         q.push(root);
-        parentMap[root] = nullptr;
 
+        TreeNode* startNode = nullptr;
+
+        // Step 1: BFS to find start node and build parent mapping
         while (!q.empty()) {
             TreeNode* curr = q.front();
             q.pop();
 
             if (curr->val == start) {
-                targetNode = curr;
+                startNode = curr;
             }
             if (curr->left) {
-                parentMap[curr->left] = curr;
+                par[curr->left] = curr;
                 q.push(curr->left);
             }
             if (curr->right) {
-                parentMap[curr->right] = curr;
+                par[curr->right] = curr;
                 q.push(curr->right);
             }
         }
-        return targetNode;
-    }
-    int burnTree(TreeNode* startNode, unordered_map<TreeNode*, TreeNode*>& parentMap) {
-        unordered_map<TreeNode*, bool> visited;
-        queue<TreeNode*> q;
-        q.push(startNode);
-        visited[startNode] = true;
-        int time = 0;
 
+        // Step 2: BFS from start node to simulate burning
+        unordered_map<TreeNode*, bool> vis;
+        q.push(startNode);
+        vis[startNode] = true;
+
+        int ans = -1;
         while (!q.empty()) {
             int size = q.size();
-            bool burnedNewNode = false;
-
-            for (int i = 0; i < size; i++) {
+            while (size--) {
                 TreeNode* curr = q.front();
                 q.pop();
 
-                if (curr->left && !visited[curr->left]) {
-                    visited[curr->left] = true;
+                // Left child
+                if (curr->left && !vis[curr->left]) {
                     q.push(curr->left);
-                    burnedNewNode = true;
+                    vis[curr->left] = true;
                 }
-                if (curr->right && !visited[curr->right]) {
-                    visited[curr->right] = true;
+                // Right child
+                if (curr->right && !vis[curr->right]) {
                     q.push(curr->right);
-                    burnedNewNode = true;
+                    vis[curr->right] = true;
                 }
-                TreeNode* parent = parentMap[curr];
-                if (parent && !visited[parent]) {
-                    visited[parent] = true;
-                    q.push(parent);
-                    burnedNewNode = true;
+                // Parent
+                if (par[curr] && !vis[par[curr]]) {
+                    q.push(par[curr]);
+                    vis[par[curr]] = true;
                 }
             }
-            if (burnedNewNode) time++;
+            ans++;
         }
-        return time;
-    }
-    int amountOfTime(TreeNode* root, int start) {
-        unordered_map<TreeNode*, TreeNode*> parentMap;
-        TreeNode* startNode = mapParents(root, start, parentMap);
-        return burnTree(startNode, parentMap);
+
+        return ans;
     }
 };
