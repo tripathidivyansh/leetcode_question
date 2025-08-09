ae2d691e9dec9a8abf2c8f9f2ebcddf6e4f8d051
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-09 11:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (48.33%) - LeetSync

---
+++ 174-dungeon-game/dungeon-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>&dungeon, vector<vector<int>>&dp){
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+
+        if(i >= n || j >= m) return 1e9;
+        if(i == n-1 && j == m-1){
+            return max(1, 1-dungeon[i][j]);
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int right = solve(i, j+1, dungeon, dp);
+        int bottom = solve(i+1, j, dungeon, dp);
+        int minnext = min(right, bottom);
+        dp[i][j] = max(1, minnext-dungeon[i][j]);
+        return dp[i][j];
+    }
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size();
+
+        int m = dungeon[0].size();
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return solve(0, 0, dungeon, dp);
+    }
+};
