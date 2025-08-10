b893bc2e38708ec255fa86c4643e035f7f9a8b07
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-10 21:26:51 +0530

    Time: 2 ms (34.81%) | Memory: 8.1 MB (80.53%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    unordered_set<string>St;
-    void buildSet(){
-        for(int p = 0; p<31; p++){
-
-            string S = to_string(1 << p);
-            sort(S.begin(), S.end());
-            St.insert(S);
+    vector<int>getVectorcount(int n){
+        vector<int>vec(10, 0);
+        while(n){
+            vec[n%10]++;
+            n/= 10;
         }
+        return vec;
     }
     bool reorderedPowerOf2(int n) {
-        if(St.empty()){
-            buildSet();
-        }
-        string S = to_string(n);
-        sort(S.begin(), S.end());
-        return St.count(S);
+        vector<int>input = getVectorcount(n);
+        for(int p = 0; p<31; p++){
+            if(input == getVectorcount(1 << p)){
+                return true;
+            }
+        }       
+        return false;
     }
+    // unordered_set<string>St;
+    // void buildSet(){
+    //     for(int p = 0; p<31; p++){
+
+    //         string S = to_string(1 << p);
+    //         sort(S.begin(), S.end());
+    //         St.insert(S);
+    //     }
+    // }
+    // bool reorderedPowerOf2(int n) {
+    //     if(St.empty()){
+    //         buildSet();
+    //     }
+    //     string S = to_string(n);
+    //     sort(S.begin(), S.end());
+    //     return St.count(S);
+    // }
 };
 // class Solution {
 // public:
