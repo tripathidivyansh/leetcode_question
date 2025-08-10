ab7a00fa930f4e7d41bc588ae8df1c33f3b3d7bd
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-10 21:21:37 +0530

    Time: 3 ms (23.60%) | Memory: 9.6 MB (14.75%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    string getSorted(int n){
-        string S = to_string(n);
-        sort(S.begin(), S.end());
+    unordered_set<string>St;
+    void buildSet(){
+        for(int p = 0; p<31; p++){
 
-        return S;
+            string S = to_string(1 << p);
+            sort(S.begin(), S.end());
+            St.insert(S);
+        }
     }
     bool reorderedPowerOf2(int n) {
-        string S = getSorted(n);
-        for(int p = 0; p<31; p++){
-            if(S == getSorted(1 << p)){
-                return true;
-            }
+        if(St.empty()){
+            buildSet();
         }
-         return false;
+        string S = to_string(n);
+        sort(S.begin(), S.end());
+        return St.count(S);
     }
 };
+// class Solution {
+// public:
+//     string getSorted(int n){
+//         string S = to_string(n);
+//         sort(S.begin(), S.end());
+
+//         return S;
+//     }
+//     bool reorderedPowerOf2(int n) {
+//         string S = getSorted(n);
+//         for(int p = 0; p<31; p++){
+//             if(S == getSorted(1 << p)){
+//                 return true;
+//             }
+//         }
+//          return false;
+//     }
+// };
