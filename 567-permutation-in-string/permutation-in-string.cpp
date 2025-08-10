67c58b0f637ceb993b83ae56afe98f02d0614ed0
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-10 21:50:52 +0530

    Time: 1069 ms (5.05%) | Memory: 60.7 MB (6.38%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
+    // void Solve(int index, string &s1, string &s2, bool &found) {
+    //     if (found) return; 
+
+    //     if (index == s1.size()) {
+    //         if (s2.find(s1) != string::npos) {
+    //             found = true;
+    //         }
+    //         return;
+    //     }
+
+    //     for (int i = index; i < s1.size(); i++) {
+    //         swap(s1[i], s1[index]);
+    //         Solve(index + 1, s1, s2, found);
+    //         swap(s1[i], s1[index]); 
+    //     }
+    // }
+
+    // bool checkInclusion(string s1, string s2) {
+    //     bool found = false;
+    //     Solve(0, s1, s2, found);
+    //     return found;
+    // }
     bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size()) return false;
-
-        vector<int> freq1(26, 0), freq2(26, 0);
-        for(char c : s1) freq1[c - 'a']++;
-        for(int i = 0; i < s1.size(); i++) {
-            freq2[s2[i] - 'a']++;
+        int n = s1.size();
+        int m = s2.size();
+        if(n > m) return false;
+        sort(s1.begin(), s1.end());
+        for(int i = 0; i<=m-n; i++){
+            string substr = s2.substr(i, n);
+            sort(substr.begin(), substr.end());
+            if(s1 == substr)return true;
         }
-        if(freq1 == freq2) return true;
-        for(int i = s1.size(); i < s2.size(); i++) {
-            freq2[s2[i] - 'a']++;                     
-            freq2[s2[i - s1.size()] - 'a']--;        
-            if(freq1 == freq2) return true;
-        }
-
         return false;
     }
-
 };
