2609b598f2c80b17fa04668681f784dcb1cbd826
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-11 22:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.52%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,13 +9,19 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* prev = NULL;
-        while(node != NULL && node->next != NULL){
-            node->val = node->next->val;
-            prev = node;
-            node = node->next;
-        }
-        prev->next = NULL;
-        delete(node);
+        // ListNode* prev = NULL;
+        // while(node != NULL && node->next != NULL){
+        //     node->val = node->next->val;
+        //     prev = node;
+        //     node = node->next;
+        // }
+        // prev->next = NULL;
+        // delete(node);
+        // You only have 'node' pointer, which is not the last node
+        node->val = node->next->val;     
+        ListNode* temp = node->next;       
+        node->next = node->next->next;     
+        delete temp;                       
+
     }
 };
