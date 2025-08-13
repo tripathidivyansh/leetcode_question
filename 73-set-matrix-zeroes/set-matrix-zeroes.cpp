66ae14c9efa3f3cc922f56749efcfda37f6b7fe8
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-13 21:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (11.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,24 +4,24 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int>row(n,0);
-        vector<int>col(m,0);
+        vector<int>row(n, 0);
+        vector<int>coln(m, 0);
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(matrix[i][j] == 0){
                     row[i] = 1;
-                    col[j] = 1;
+                    coln[j] = 1;
                 }
             }
         }
-        for(int i= 0; i<n; i++){
+
+        for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(row[i] || col[j]){
-                    matrix[i][j]  = 0;
+                if(row[i] || coln[j]){
+                    matrix[i][j] = 0;
                 }
             }
         }
     }
+
 };
-
-
