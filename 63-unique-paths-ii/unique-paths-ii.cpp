5453d112d3eefca1d4dab6cc58f3ec19714fcfe9
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-14 01:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.37%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,25 +1,221 @@
 class Solution {
 public:
+    int Solve(int i, int j, vector<vector<int>>&obstacleGrid, vector<vector<int>>&dp){
+        int  n = obstacleGrid.size();
+        int  m  = obstacleGrid[0].size();
+        if(i < 0 || j <  0 || i>= n || j>= m) return 0;
+        if(obstacleGrid[i][j] == 1) return 0;
 
-    int solve(int i, int j, int n, int m, vector<vector<int>>&obstacleGrid, vector<vector<int>>&dp){
-        if(i>=n || j >= m || obstacleGrid[i][j] == 1){
-            return 0;
-        }
-        if(i == n - 1 && j == m - 1){
-            return 1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int right = solve(i+1, j, n, m, obstacleGrid, dp);
-        int bottom = solve(i, j+1, n, m, obstacleGrid, dp);
-        dp[i][j] = right+bottom;
-        return dp[i][j];
+        if(i == n-1 && j == m-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        int bottom = Solve(i+1, j, obstacleGrid, dp);
+        int right= Solve(i, j+1, obstacleGrid, dp);
+        return dp[i][j] = bottom + right;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
+
         vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        return solve(0, 0, n, m, obstacleGrid, dp);
+        return Solve(0, 0, obstacleGrid, dp);
     }
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
