ae7a1798360ab57ff046bf56605bc09e2d77f2c0
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-14 01:31:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,26 +1,18 @@
 class Solution {
-public:
-    int solve(int&m, int&n, int i, int j, vector<vector<int>>&dp){
-        if(i >= m || j >= n){
+public: 
+    int Solve(int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i < 0 || j < 0 || i >= m || j >= n){
             return 0;
         }
-        if(i == m - 1 && j == n - 1){
-            return 1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int right = solve(m, n, i, j + 1, dp);
-        int down = solve(m, n,  i + 1, j, dp); 
-
-        return dp[i][j] = right + down; 
+        if(i == m-1 &&  j == n-1) return 1;
+        if(dp[i][j] != -1)return dp[i][j];
+        int right= Solve(i, j+1, m, n, dp);
+        int bottom = Solve(i+1, j,m, n, dp);
+        return dp[i][j] = right + bottom;;
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1)); 
-        
-
-        return solve(m, n,0, 0, dp); 
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        return Solve(0, 0, m, n, dp);
     }
 };
