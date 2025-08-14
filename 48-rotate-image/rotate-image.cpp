79c817f0f1afdea79d8da23917a4a408d195c282
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-15 02:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.71%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -5,7 +5,7 @@ public:
         int m = matrix[0].size();
 
         for(int i = 0; i<n; i++){
-            for(int j = i+1; j<m; j++){
+            for(int j = 0; j<i; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
