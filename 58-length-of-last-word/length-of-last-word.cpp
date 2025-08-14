3aeb0aaa107c0868a44303931f0d410aba0940c7
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-14 19:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (9.67%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string ans = "";
-        for(int i = s.size() -1; i>= 0; i--){
+        // string ans = "";
+        // for(int i = s.size() -1; i>= 0; i--){
             
+        //     if(s[i] != ' '){
+        //         ans += s[i];
+        //     }else if(ans.size() > 0){
+        //         break;
+        //     }
+        // }
+        
+        // return ans.size();
+        int lenght = 0;
+        for(int i = s.size()-1; i>= 0; i--){
+
             if(s[i] != ' '){
-                ans += s[i];
-            }else if(ans.size() > 0){
+                lenght++;
+            }else if(lenght > 0){
                 break;
             }
         }
-        
-        return ans.size();
+        return lenght;
     }
 };
 
