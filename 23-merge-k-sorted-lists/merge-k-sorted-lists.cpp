20d1f4d0987921c76564053c01d6a570e8f0bd25
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-15 03:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (66.55%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,28 +10,45 @@
  */
 class Solution {
 public:
+    ListNode* mergetwoSorted(ListNode* l1 , ListNode* l2){
+        if(!l1 ) return l2;
+        if(!l2) return l1;
+        if(l1->val <= l2->val){
+            l1->next = mergetwoSorted(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = mergetwoSorted(l1, l2->next);
+            return l2;
+        }
+        return NULL;
+    }
+    ListNode* partion(int Start, int end, vector<ListNode*>& lists){
+        if(Start > end){
+            return NULL;
+        }
+        if(Start == end){
+            return lists[Start];
+        }
+        int mid = Start + (end-Start)/2;
+        ListNode* l1 = partion(Start, mid, lists);
+        ListNode* l2 = partion(mid+1, end, lists);
+        return mergetwoSorted(l1, l2);
+
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
-        for (int i = 0; i < lists.size(); i++) {
-            if (lists[i]) {
-                pq.push({lists[i]->val, lists[i]});
-            }
-        }
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-
-        while (!pq.empty()) {
-            pair<int, ListNode*> p = pq.top();
-            pq.pop();
-
-            ListNode* node = p.second;
-            temp->next = node;
-            temp = temp->next;
-
-            if (node->next) {
-                pq.push({node->next->val, node->next});
-            }
-        }
-        return dummy->next;
+        int k = lists.size();
+        if(k == 0) return NULL;
+        return partion(0, k-1, lists);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
