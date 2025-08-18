2b7516c635787bcd99c96ac4aa621ece727a1a5c
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-18 23:52:58 +0530

    Time: 59 ms (30.59%) | Memory: 18.7 MB (36.33%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -9,19 +9,41 @@ public:
         //     }
         // }
         // return false;
-        int i = 0, j = matrix[0].size()-1;
-        while(i < matrix.size() && j >= 0){
-            if(matrix[i][j] == target){
-                return true;
-            }else if(matrix[i][j] < target){
-                i++;
-            }else{
-                j--;
+        // int i = 0, j = matrix[0].size()-1;
+        // while(i < matrix.size() && j >= 0){
+        //     if(matrix[i][j] == target){
+        //         return true;
+        //     }else if(matrix[i][j] < target){
+        //         i++;
+        //     }else{
+        //         j--;
+        //     }
+        // }
+        // return false;
+        int n = matrix.size(), m = matrix[0].size();
+        int low = 0, high = n * m - 1;
+        for (int i = 0; i < n; i++) {
+            
+            if (target < matrix[i][0] || target > matrix[i][m - 1]) {
+                continue;
+            }
+            
+            int low = 0, high = m - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (matrix[i][mid] == target) return true;
+                if (matrix[i][mid] < target) low = mid + 1;
+                else high = mid - 1;
             }
         }
         return false;
     }
 };
+
+
+
+
+
 
 
 
