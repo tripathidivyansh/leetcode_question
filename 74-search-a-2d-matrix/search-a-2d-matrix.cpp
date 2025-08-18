b6e75197704b5d6a344e5e8f5344daf32681235e
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-18 23:15:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (42.69%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,11 +4,22 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(matrix[i][j] == target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        int i = 0, j = m-1;
+        while(i < n &&  j >= 0){
+            if(matrix[i][j] == target){
+                return true;
+            }else if(matrix[i][j] < target){
+                i++;
+            }else{
+                j--;
             }
         }
         return false;
