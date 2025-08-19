3345923a45fcdaf7a10f35b08f41a422bbaddef6
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-19 22:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (79.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,37 +1,21 @@
-// class Solution {
-// public:
-//     bool isPalindromeHelper(int l, int r, string s){
-
-//         if(l>= r) return true;
-//         if(!isalnum(s[l]))return isPalindromeHelper(l+1,r,s);
-//         if(!isalnum(s[r]))return isPalindromeHelper(l,r-1,s);
-//         if(tolower(s[l]) != tolower(s[r])) return false;
-//         return isPalindromeHelper(l+1, r-1, s);
-//     }
-//     bool isPalindrome(string s) {
-//         return isPalindromeHelper(0, s.size()-1, s);
-//     }
-// };
-
-class Solution{
+class Solution {
 public:
-    bool isPalindrome(string s){
-        int left = 0;
-        int right = s.size()-1;
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size()-1;
+        while(l < r){
 
-        while(left< right){
-            if(!isalnum(s[left])){
-                left++;
-            }else if(!isalnum(s[right])){
-                right--;
-            }else if(tolower(s[left]) != tolower(s[right])){
-                return false;
-            }
-            else{
-                left++;
-                right--;
-            }
+            while(l < r && !isalnum(s[l])) l++;
+            while(l < r && !isalnum(s[r])) r--;
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            
+            l++;
+            r--;
         }
         return true;
     }
 };
+
+
+
+
+
