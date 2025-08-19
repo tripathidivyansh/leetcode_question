128bef0ad523d469c03c4c588d3a75366629663f
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-20 04:15:53 +0530

    Time: 4 ms (88.23%) | Memory: 22.8 MB (96.20%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isPossilbe(vector<int>&piles, int mid, int h){
         int actualhourS = 0;
-        for(int & x: piles){
+        for(int &x : piles){
 
             actualhourS += x / mid;
-            if(x% mid != 0){
+            if(x % mid != 0){
                 actualhourS++;
             }
         }
@@ -15,20 +15,19 @@ public:
         return false;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
         int l = 1, r = *max_element(piles.begin(), piles.end());
+        int reSutlt = r;
 
-        int reSult = r;
         while(l < r){
             int mid = l + (r-l)/2;
             if(isPossilbe(piles, mid, h)){
-                reSult = mid;
-
+                reSutlt = mid;
                 r = mid;
             }else{
                 l = mid+1;
             }
         }
-        return reSult;
+        
+        return reSutlt;
     }
 };
