004cf3e2f1f4c2de8a9673df2d440ea6964dc188
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-19 19:21:31 +0530

    Time: 3 ms (2.00%) | Memory: 15.3 MB (33.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    int getPivot(vector<int>& nums, int n) {
-        int S = 0, e = n - 1;
-        while (S < e) {
-            int mid = S + (e - S) / 2;
-            if (nums[mid] >= nums[0]) {
-                S = mid + 1;
-            } else {
-                e = mid;
-            }
-        }
-        return S;
-    }
-
-    int binarySearch(vector<int>& nums, int S, int e, int target) {
-        while (S <= e) {
-            int mid = S + (e - S) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                S = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return -1;
-    }
-
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int pivot = getPivot(nums, n);
-
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            return binarySearch(nums, pivot, n - 1, target);
-        } else {
-            return binarySearch(nums, 0, pivot - 1, target);
+        int ans = -1;
+        for(int i = 0; i<nums.size(); i++){
+            
+            if(nums[i] == target){
+                ans =  i;
+                break;
+            }
         }
+        return ans;
     }
 };
+
+
+
+
+
+
+
