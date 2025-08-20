26ded55516a653cd26a7863427e00eab3b6227e3
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-20 07:40:30 +0530

    Time: 24 ms (7.73%) | Memory: 31.6 MB (6.35%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (matrix[i][j] == 0) return dp[i][j] = 0;
+
+        int up = solve(matrix, i-1, j, dp);
+        int left = solve(matrix, i, j-1, dp);
+        int diag = solve(matrix, i-1, j-1, dp);
+
+        return dp[i][j] = 1 + min({up, left, diag});
+    }
+
     int countSquares(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0];
-        }
-        for (int j = 0; j < m; j++) {
-            dp[0][j] = matrix[0][j];
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 1) {
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                } else {
-                    dp[i][j] = 0;
-                }
-            }
-        }
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
         int sum = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                sum += dp[i][j];
+                sum += solve(matrix, i, j, dp);
             }
         }
         return sum;
     }
 };
+
+
