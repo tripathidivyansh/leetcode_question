a45fbb8a830572bf3691aa9a6de67d201aeb45a2
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-21 03:20:40 +0530

    Time: 11 ms (12.15%) | Memory: 32.1 MB (64.14%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int S = 0;
-        int mini = INT_MAX;
+        // int S = 0;
+        // int mini = INT_MAX;
 
-        for(int i = 0; i<nums.size(); i++){
-            S += nums[i];
-            mini = min(mini, nums[i]);
+        // for(int i = 0; i<nums.size(); i++){
+        //     S += nums[i];
+        //     mini = min(mini, nums[i]);
+        // }
+        // return S - (mini * nums.size());
+        sort(nums.begin(), nums.end());
+        int moves = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            moves += nums[i] - nums[0];
         }
-        return S - (mini * nums.size());
+        return moves;
     }
 };
