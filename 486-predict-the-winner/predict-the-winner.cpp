0b9a76242f12a55cb202d255cc921eba357d46bb
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-26 04:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (27.95%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int Solve(int i, int j, vector<int>&nums){
+    int Solve(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
         if(i == j) return nums[i];
-        int leftpick = nums[i] - Solve(i+1, j, nums);
-        
-        int rightpick = nums[j] - Solve(i, j-1, nums);
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int leftpick = nums[i] - Solve(i+1, j, nums, dp);
+        int rightpick = nums[j] - Solve(i, j-1, nums, dp);
         int maxi = max(leftpick, rightpick);
-        return maxi;
+        dp[i][j] = maxi;
+        return dp[i][j];
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int Score =  Solve(0, n-1, nums);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        int Score =  Solve(0, n-1, nums, dp);
         if(Score >= 0){
             return true;
         }else{
-            
             return false;
         }
     }
