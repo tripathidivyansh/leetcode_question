fe319875487c0a869bc90d6776c80835dce9493a
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-25 22:29:53 +0530

    Time: 94 ms (55.40%) | Memory: 217.3 MB (23.84%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
-    bool Solve(int curr_index, int prevjump, vector<int>&stones, vector<vector<int>>&dp, unordered_map<int,int>&mp){
-        if(curr_index >= stones.size()-1) return true;
+    bool Solve(int curr_index, int prevjump, vector<int>&stones, unordered_map<int,int>&mp, vector<vector<int>>&dp){
+        if(curr_index >= stones.size() - 1) return true;
+        bool reSult = false;
+
         if(dp[curr_index][prevjump] != -1) return dp[curr_index][prevjump];
-        
-        bool result = false;
         for(int nextjump = prevjump-1; nextjump <= prevjump+1; nextjump++){
             if(nextjump > 0){
-                int nextStone = stones[curr_index] + nextjump;
-                if(mp.find(nextStone) != mp.end()){
-                    result = result || Solve(mp[nextStone], nextjump, stones, dp, mp);
-                }
+              int nextStone =  stones[curr_index] + nextjump;
+              if(mp.find(nextStone) != mp.end()){
+                reSult = reSult ||  Solve(mp[nextStone], nextjump, stones, mp, dp);
+              }
             }
         }
-        return dp[curr_index][prevjump] = result;
+        dp[curr_index][prevjump] = reSult;
+        return reSult;
     }
     bool canCross(vector<int>& stones) {
         int n = stones.size();
+        unordered_map<int,int>mp;
+        
         if(stones[1] != 1) return false;
-        unordered_map<int,int> mp;
-        for(int i = 0; i < n; i++){
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        for(int i = 0; i<stones.size(); i++){
             mp[stones[i]] = i;
         }
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return Solve(0, 0, stones, dp, mp);
+        return Solve(0, 0, stones, mp, dp);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
