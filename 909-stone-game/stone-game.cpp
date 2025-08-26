a6dee898b01e6cac92bf4e099476c3dcab7f1769
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-26 21:48:42 +0530

    Time: 13 ms (24.37%) | Memory: 19.7 MB (11.82%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int Solve(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
-        if(i == j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
+    int Solve(int i, int j, vector<int>&piles, vector<vector<int>>&dp){
+        if(i > j) return 0;
+        if(i == j) return piles[i];
 
-        int leftpick = nums[i] - Solve(i+1, j, nums, dp);
-        int rightpick = nums[j] - Solve(i, j-1, nums, dp);
-        int maxi = max(leftpick, rightpick);
-        dp[i][j] = maxi;
-        return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int take_i = piles[i] + min(Solve(i+2, j, piles, dp), Solve(i+1, j-1, piles, dp));
+        int take_j = piles[j] + min(Solve(i, j-2, piles, dp), Solve(i+1, j-1, piles, dp));
+        return dp[i][j] =  max(take_i, take_j);
+        
     }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
+        int totalSum = accumulate(piles.begin(), piles.end(), 0);
         vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        int Score = Solve(0, n-1, piles, dp);
-        if(Score > 0){
-
+        int firSt = Solve(0, n-1, piles, dp);
+        int Second = totalSum - firSt;
+        if(firSt >= Second){
             return true;
-        }else{
-            return false;
         }
-        
+        return false;
     }
 };
