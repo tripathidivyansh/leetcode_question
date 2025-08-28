99fb07157221437c8139458762215ca06b5d2365
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-28 22:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (11.21%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        int Sum = 0;
-        sort(answers.begin(), answers.end());
+        // int Sum = 0;
+        // sort(answers.begin(), answers.end());
 
-        for(int i = 0; i<answers.size(); i++){
-            int count = 1;
-            while(i+1 < answers.size() && answers[i] == answers[i+1]){
-                count++;
-                i++;
-            }
-            int groupSize = answers[i] + 1;
-            int group = (count + groupSize - 1)/ groupSize;
-            Sum += group* groupSize;
-        }
+        // for(int i = 0; i<answers.size(); i++){
+        //     int count = 1;
+        //     while(i+1 < answers.size() && answers[i] == answers[i+1]){
+        //         count++;
+        //         i++;
+        //     }
+        //     int groupSize = answers[i] + 1;
+        //     int group = (count + groupSize - 1)/ groupSize;
+        //     Sum += group* groupSize;
+        // }
         
-        return Sum;
+        // return Sum;
+        unordered_map<int,int>mp;
+        for(int &x: answers){
+            mp[x]++;
+        }
+        int total = 0;
+        for(auto &it : mp){
+            int x = it.first;
+            int count = it.second;
+            int groupSize = x + 1;
+            int groupS = ceil((double) count / groupSize);
+            total += groupS * (groupSize);
+        }
+        return total;
     }
 };
