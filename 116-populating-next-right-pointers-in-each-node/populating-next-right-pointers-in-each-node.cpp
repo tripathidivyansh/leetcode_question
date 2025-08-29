bb4f1acfd208c3ed36edf929240c6ae9033fd657
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-08-29 18:36:20 +0530

    Time: 18 ms (15.73%) | Memory: 19 MB (71.07%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -17,27 +17,71 @@ public:
 */
 class Solution {
 public:
+    void Solve(Node* leftNode, Node* rightNode){
+        if(!leftNode || !rightNode) return;
+        leftNode->next = rightNode;
+        Solve(leftNode->left, leftNode->right);
+        Solve(rightNode->left, rightNode->right);
+        Solve(leftNode->right, rightNode->left);
+    }
     Node* connect(Node* root) {
+        // if(!root) return NULL;
+        // queue<Node*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int Size = q.size();
+        //     Node* prev = nullptr;
+        //     for(int i = 0; i<Size; i++){
+        //         Node* front = q.front();
+        //         q.pop();
+        //         if(prev){
+        //             prev->next = front;
+        //         }
+        //         prev = front;
+        //         if(front->left)q.push(front->left);
+        //         if(front->right) q.push(front->right);
+        //     }
+        // }
+        // return root;
         if(!root) return NULL;
-        queue<Node*>q;
-        q.push(root);
-        while(!q.empty()){
-            int Size = q.size();
-            Node* prev = nullptr;
-            for(int i = 0; i<Size; i++){
-                Node* front = q.front();
-                q.pop();
-                if(prev){
-                    prev->next = front;
-                }
-                prev = front;
-                if(front->left)q.push(front->left);
-                if(front->right) q.push(front->right);
-            }
-        }
+        Solve(root->left, root->right);
         return root;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
