de078d876cb0c4804705aa8e26cf5ce757fd09b9
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-04 19:47:48 +0530

    Time: 7 ms (41.26%) | Memory: 23.8 MB (80.04%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -8,6 +8,7 @@ public:
         vector<int>temp = intervals[0];
         for(int i = 1; i<intervals.size(); i++){
             if(intervals[i][0] <= temp[1]){
+
                 temp[1] = max(intervals[i][1], temp[1]);
             }else{
                 merged.push_back(temp);
