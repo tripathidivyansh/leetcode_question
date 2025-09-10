fed4a0ebc8024c1008f71dc5bfb4013fe40cb28a
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-10 15:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (86.43%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,73 +1,36 @@
-// class MyStack {
-// public:
-//     queue<int>q1,q2;
-//     MyStack() {
-        
-//     }
-//     void push(int x) {
-//         while(!q1.empty()){
-//             q2.push(q1.front());
-//             q1.pop();
-//         }
-//         q1.push(x);
-//         while(!q2.empty()){
-//             q1.push(q2.front());
-//             q2.pop();
-//         }
-//     }
-    
-//     int pop() {
-//         int val = q1.front();
-//         q1.pop();
-//         return val;
-//     }
-    
-//     int top() {
-//         return q1.front();
-//     }
-    
-//     bool empty() {
-//         return q1.empty();
-//     }
-// };
-
-
-
 class MyStack {
 public:
-    queue<int> q1, q2;
-
-    MyStack() {}
-
+    queue<int>q1,q2;
+    MyStack() {
+        
+    }
     void push(int x) {
-        // Push the new element onto q2
-        q2.push(x);
-
-        // Push all the elements of q1 to q2
-        while (!q1.empty()) {
+        while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
+            
         }
-
-        // Swap the names of q1 and q2
-        swap(q1, q2);
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
     }
-
+    
     int pop() {
         int val = q1.front();
         q1.pop();
         return val;
     }
-
+    
     int top() {
         return q1.front();
     }
-
+    
     bool empty() {
         return q1.empty();
     }
 };
-
 
 /**
  * Your MyStack object will be instantiated and called as such:
