078d4d3ffa563ea36439d5b26fc2c7cc75d96086
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-11 15:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (35.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,15 +1,49 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> ans;
+//         unordered_map<int, int> mp;
+
+//         for (auto x : nums) {
+//             mp[x]++;
+//         }
+
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+//         for (auto& x : mp) {
+//             pq.push(make_pair(x.second, x.first));
+//             if (pq.size() > k) {
+//                 pq.pop();
+//             }
+//         }
+
+//         while (!pq.empty()) {
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+
+//         reverse(ans.begin(), ans.end());
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        for(int i : nums) mp[i]++;
-
-        priority_queue<pair<int,int>>pq;
-        for(auto i : mp){
-            pq.push({i.second, i.first});
+        int n = nums.size();
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for (auto x : nums) {
+            mp[x]++;
         }
-        vector<int>ans;
-        while(k--){
+        priority_queue<pair<int, int>> pq;
+        for (auto x : mp) {
+            pq.push(make_pair(x.second, x.first));
+        }
+        for (int i = 0; i < k; ++i) {
             ans.push_back(pq.top().second);
             pq.pop();
         }
