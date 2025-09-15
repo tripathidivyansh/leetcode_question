9ada5783374755c7121718bca5c9f4499b3a4ce5
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-15 15:29:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (29.65%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -1,8 +1,221 @@
+// class Solution {
+// public:
+//     int findGCD(vector<int>& nums) {
+//         int miniE = *min_element(nums.begin(), nums.end());
+//         int maxE = *max_element(nums.begin(), nums.end());
+//         return __gcd(miniE, maxE);
+//     }
+// };
 class Solution {
 public:
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
     int findGCD(vector<int>& nums) {
         int miniE = *min_element(nums.begin(), nums.end());
+
         int maxE = *max_element(nums.begin(), nums.end());
-        return __gcd(miniE, maxE);
+        return gcd(miniE, maxE);
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
