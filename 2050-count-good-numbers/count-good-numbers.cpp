5f7b1c56c35de676d84862b670a0dea28535e733
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-15 15:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.56%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,24 +1,221 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    long long solve(long long base, long long exp) {
-        if (exp == 0) return 1;
+    long long solve(long long base, long long expoential){
+        if(expoential == 0){
+            return 1;
+        }
+        long long half = solve(base, expoential/2);
+        long long result = (half*half) % mod;
 
-        long long half = solve(base, exp / 2);
-        long long result = (half * half) % mod;
-
-        if (exp % 2 == 1) {
-            result = (result * base) % mod;
+        if(expoential % 2 == 1){
+            result = (result *base) % mod;
         }
         return result;
     }
     int countGoodNumbers(long long n) {
-        long long even = n / 2 + n % 2;
-        long long odd = n / 2;
+        long even = n/2 + n % 2;
+        long  odd = n/2;
 
-        long long evenpart = solve(5, even);
-        long long oddpart = solve(4, odd);
+        long evenpart = solve(5, even);
+        long oddpart = solve(4, odd);
 
         return (evenpart * oddpart) % mod;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
