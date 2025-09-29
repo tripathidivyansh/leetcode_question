3b37f77731d095d1daff2a47b5946f6ac1c8c0e4
Divyansh_Tripathi_07 <143827072+tripathidivyansh@users.noreply.github.com>
2025-09-29 20:36:55 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        if(n == 0) return 0;
-        int water = 0;
         int left = 0, right = height.size()-1;
-        int leftmax = 0 , rightmax = 0;
+        int leftmax = 0, rightmax = 0, water = 0;
 
-        while(left<=right){
-            if(height[left] <height[right]){
+        while(left <= right){
+            if(height[left] < height[right]){
                 if(height[left] > leftmax){
                     leftmax = height[left];
+
                 }else{
-                    water += leftmax- height[left];
+                    water += leftmax - height[left];
                 }
                 left++;
             }else{
@@ -21,9 +19,11 @@ public:
                 }else{
                     water += rightmax - height[right];
                 }
+
                 right--;
             }
         }
+        
         return water;
     }
 };
@@ -53,120 +53,3 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         int n = height.size();
-//         if(n==0) return 0;
-//         int leftmax = 0, rightmax =0;
-//         int left = 0, right = n-1;
-//         int water = 0;
-//         while(left <=right){
-//             if(height[left] <height[right]){
-//                 if(height[left] > leftmax){
-//                     leftmax = height[left];
-//                 }else{
-//                     water += leftmax-height[left];
-//                 }
-//                 left++;
-//             }else{
-//                 if(height[right] >= rightmax){
-//                     rightmax = height[right];
-//                 }else{
-//                     water += rightmax-height[right];
-//                 }
-//                 right--;
-//             }
-
-//         }
-//         return water;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     int trap(vector<int>& height) {
-//         int n = height.size();
-//         int water = 0;
-
-//         for(int i = 0; i<n; i++){
-//             int j = i;
-//             int leftmax = 0, rightmax = 0;
-//             while(j>=0){
-//                 leftmax = max(leftmax, height[j]);
-//                 j--;
-//             }
-//             j = i;
-//             while(j<n){
-//                 rightmax = max(rightmax, height[j]);
-//                 j++;
-//             }
-//             water += min(leftmax, rightmax)-height[i];
-//         }
-//         return water;
-//     }
-// };
